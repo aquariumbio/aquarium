a99ed77175473bc11f629be4c24a7be050bd8492
Eric Klavins <klavins@uw.edu>
2013-06-13 09:08:26 -0700

    finished static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+
   def home
   end
 
   def help
   end
+
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Bioturk::Application.routes.draw do
 
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
 
 end
