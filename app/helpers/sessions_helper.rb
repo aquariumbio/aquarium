eec3d50320a04eb5b26fb9331686dd186a7125d2
Eric Klavins <klavins@uw.edu>
2013-06-20 08:33:39 -0700

    made json requests for objects not require authorization (which is probably a bad idea, but I need it to test the pdl object class

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,7 +22,7 @@ module SessionsHelper
   end
 
   def signed_in_user
-    unless signed_in?
+    unless signed_in? || request.format == 'json'
       store_location
       redirect_to signin_url, notice: "Please sign in."
     end
