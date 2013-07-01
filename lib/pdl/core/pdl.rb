04c44dd0c05bd6d3be594f9d58989ce6c22b7b4b
Eric Klavins <klavins@uw.edu>
2013-07-01 14:59:33 -0700

    require pdl

---
+++ lib/pdl/core/pdl.rb
@@ -0,0 +1,6 @@
+require './lib/pdl/core/protocol'
+require './lib/pdl/core/scope'
+require './lib/pdl/core/interpreter'
+require './lib/pdl/core/pdl_item'
+
+Dir["./lib/pdl/core/*instruction.rb"].each { |file| require file }
--- lib/pdl/lib/pdl.rb
+++
@@ -1,6 +0,0 @@
-require './lib/protocol'
-require './lib/scope'
-require './lib/interpreter'
-require './lib/pdl_item'
-
-Dir["./lib/*instruction.rb"].each { |file| require file }
