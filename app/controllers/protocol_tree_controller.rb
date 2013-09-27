1e102ea57e7e41009fb1782ce540186dce78a8c8
Eric Klavins <klavins@uw.edu>
2013-09-27 09:57:46 -0700

    Recent protocols

--- app/controllers/protocol_tree_controller.rb
+++ app/controllers/protocol_tree_controller.rb
@@ -108,6 +108,14 @@ class ProtocolTreeController < ApplicationController
   end
 
   def recent
+    @jobs = Job.where("user_id==:user AND created_at >= :date", { user: current_user, date: Time.now.weeks_ago(1) } ).reverse_order
+    @recents = []
+    @jobs.each do |j|
+      @recents.push( { path: j.path, sha: j.sha, args: args = (JSON.parse(j.state))['stack'].first.reject { |k,v| k == 'user_id' } } )
+    end
+
+    @recents.uniq!
+      
   end
 
   def raw
