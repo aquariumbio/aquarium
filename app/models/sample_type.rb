b73f94d0490eefae23d05378a8ef037aac4b5ea7
Eric Klavins <klavins@uw.edu>
2013-10-23 17:25:18 -0700

    oops

--- app/models/sample_type.rb
+++ app/models/sample_type.rb
@@ -2,7 +2,7 @@ class SampleType < ActiveRecord::Base
 
   attr_accessible :description, :field1name, :field1type, :field2name, :field2type, :field3name, 
                                 :field3type, :field4name, :field4type, :field5name, :field5type, 
-                                :field6name, :field6type, 
+                                :field6name, :field6type, :field7name, :field7type, :field8name, :field8type,  
                                 :name
 
   has_many :samples
