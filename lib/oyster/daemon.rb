7d637e191d131601cc8d8033e75ff64f59b14200
Eric Klavins <klavins@uw.edu>
2013-12-20 10:54:25 -0800

    Update the daemon that should prevent it from crashing when the istc server doesn't respond

--- lib/oyster/daemon.rb
+++ lib/oyster/daemon.rb
@@ -1,49 +1,79 @@
 
+  #
+  # Aquarium metacol daemon
+  #
+  # This daemon periodically updates all active metacols. 
+  #
+
   def metacol_daemon
+
+    puts "#{Time.now}: Starting Aquarium daemon."
 
     while true
 
-      sleep 1
+      sleep 3
 
-      Metacol.where("status = 'RUNNING'").each do |process|
+      # Build the list of active metacols. The db query (Metacol.where) is in a begin...rescue on the off chance
+      # that the db server does not respond to the query, which actually seems to happen once every few days.
 
-        unless process.num_pending_jobs > 10
+      is_valid_query = true
 
-          blob = Blob.get process.sha, process.path
-          content = blob.xml
+      begin 
+        procs = Metacol.where("status = 'RUNNING'")
+        l = procs.length # This line forces active record to query the db here, instead of at procs.each below
+      rescue Exception => e
+        puts "#{Time.now}: Could not get current processes from Database Server: " + e.message.split('[')[0]
+        is_valid_query = false
+      end
 
-          error = false
-          begin
-            m = Oyster::Parser.new(content).parse
-          rescue Exception => e
-            error = true
-            process.message = "Error parsing #{process.path}: " + e
-            puts process.message
-            process.status = "ERROR"
-            process.save
-          end
+      if is_valid_query
 
-          if !error
+        procs.each do |process|
 
-            m.set_state( JSON.parse process.state, :symbolize_names => true )
-            m.id = process.id
+          unless process.num_pending_jobs > 10 # to keep poorly written metacols from spiraling out of control
+                                               # we limit the number of pending jobs they can have to 11
+
+            # Get the metacol and parse it, checking for parse errors along the way
+
+            blob = Blob.get process.sha, process.path
+            content = blob.xml
+            error = false
 
             begin
-              m.update
+              m = Oyster::Parser.new(content).parse
             rescue Exception => e
-              process.message = "On update: " + e.message.split('[')[0]
+              error = true
+              process.message = "#{Time.now}: Error while parsing #{process.path}: " + e.message.split('[')[0]
               puts process.message
               process.status = "ERROR"
               process.save
             end
 
-            process.state = m.state.to_json
+            if !error 
 
-            if m.done?
-              process.status = "DONE"
+              # Parsing was successful, so update the process.
+
+              m.set_state( JSON.parse process.state, :symbolize_names => true )
+              m.id = process.id
+
+              begin
+                m.update
+              rescue Exception => e
+                process.message = "#{Time.now}: Error on update: " + e.message.split('[')[0]
+                puts process.message
+                process.status = "ERROR"
+                process.save
+              end
+
+              process.state = m.state.to_json
+
+              if m.done?
+                process.status = "DONE"
+              end
+
+              process.save
+
             end
-
-            process.save
 
           end
 
