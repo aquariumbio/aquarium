712ee5d0d9b0ac48be310ae104be9b710696aa48
Eric Klavins <klavins@uw.edu>
2014-01-07 08:09:05 -0800

    forgot to add user groups

--- lib/tasks/multiple_users.rb
+++ lib/tasks/multiple_users.rb
@@ -18,12 +18,19 @@ names = [
 
 names.each do |name|
 
-  u = User.new
-  u.login = name[0].downcase
-  u.name = "#{name[1]} #{name[0]}"
-  u.password = "aqua#{name[0].downcase}"
-  u.password_confirmation = "aqua#{name[0].downcase}"
-  u.save
+  #u = User.new
+  #u.login = name[0].downcase
+  #u.name = "#{name[1]} #{name[0]}"
+  #u.password = "aqua#{name[0].downcase}"
+  #u.password_confirmation = "aqua#{name[0].downcase}"
+  #u.save
+
+  login = name[0].downcase
+  u = User.find_by_login(login)
+  g = Group.new
+  g.name = login
+  g.description = "A group containing only user #{login}"
+  g.save
 
 end 
 
