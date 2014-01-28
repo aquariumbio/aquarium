ad611ce2e75a6371193005702f09107a990c3ca5
Eric Klavins <klavins@uw.edu>
2014-01-28 13:12:01 -0800

    highlighting in table rows + link to sample on sample name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "BIOTURK"
+    base_title = "AQUARIUM"
     if page_title.empty?
       base_title
     else
