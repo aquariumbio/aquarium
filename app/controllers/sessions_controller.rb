042dcc61b1093b89920078354d6f6d7a45a7001c
Eric Klavins <klavins@uw.edu>
2014-03-19 13:42:54 -0700

    bioturk => aquarium

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by_login(params[:session][:login].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      flash[:success] = "Welcome to BioTurk, #{user.login}. Your biological and technological distinctiveness will be added to our own. Resistance is futile."
+      flash[:success] = "Welcome to the Aquarium, #{user.login}. Your biological and technological distinctiveness will be added to our own."
       redirect_back_or user
     else
       flash.now[:error] = 'Invalid login/password combination'
