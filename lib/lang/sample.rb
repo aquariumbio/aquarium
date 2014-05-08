ca5972537fcbe3a7b0237ee183c1d5a2e83f0e7f
Eric Klavins <klavins@uw.edu>
2014-05-08 16:53:02 -0700

    ids show for samples and html_safe in steps

--- lib/lang/sample.rb
+++ lib/lang/sample.rb
@@ -55,19 +55,35 @@ module Lang
       end
     end
 
-    def info pdl_item
+    def info x
 
-      if pdl_item.class == Hash && pdl_item[:id]
-        i = Item.find_by_id(pdl_item[:id])
+      if x.class == Hash && x[:id]
+
+        i = Item.find_by_id(x[:id])
+
         if !i
-          raise "Could not find item #{pdl_item[:id]} in argument passed to 'info'"
+          raise "Could not find item #{x[:id]} in argument passed to 'info'"
         end
         if !i.sample
-          raise "Item #{pdl_item[:id]} in argument passed to 'info' is not a sample"
+          raise "Item #{x[:id]} in argument passed to 'info' is not a sample"
         end
+
         return i.sample.attributes.symbolize_keys
+
+      elsif x.class == Fixnum
+
+        s = Sample.find_by_id(x)
+
+        if !s
+          raise "Could not find sample #{x} in argument passed to 'info'"
+        end
+
+        return s.attributes.symbolize_keys
+
       else
+
         raise "Argument passed to 'info' is not an item"
+
       end
 
     end
