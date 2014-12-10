f1e6b03d37a6cbbb6b78beb0695edd50c253deda
Eric Klavins <klavins@uw.edu>
2014-12-09 22:01:27 -0800

    session names are deployment specific

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Bioturk::Application.config.session_store :cookie_store, key: '_bioturk_session'
+Bioturk::Application.config.session_store :cookie_store, key: "_aquarium_#{Rails.env}_session"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
