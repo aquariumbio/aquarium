7d27b17c4154b4abb580df6896f1058ed1228f74
Eric Klavins <klavins@uw.edu>
2015-01-25 17:23:10 -0800

    wizard work

--- app/models/wizard.rb
+++ app/models/wizard.rb
@@ -1,10 +1,12 @@
 class Wizard < ActiveRecord::Base
+
   attr_accessible :name, :specification, :description
+
   validates :name, presence: true
   validates :description, presence: true
   validates_uniqueness_of :name
 
-  def spec 
+  def spec # converts the specification into a reasonable ruby object
 
     if self.specification
 
@@ -17,7 +19,6 @@ class Wizard < ActiveRecord::Base
       end
 
       s[:fields] = t
-
       s
 
     else
@@ -26,6 +27,38 @@ class Wizard < ActiveRecord::Base
 
     end
 
+  end
+
+  def object_types
+    ObjectType.where(prefix: self.name)
+  end
+
+  def items
+    Item.joins(:object_type).where(object_types: { prefix: self.name })
+  end
+
+  def locations
+    Item.joins(:object_type)
+      .where(object_types: { prefix: self.name })
+      .select("location")
+      .collect { |i| i.location }
+  end
+
+  def maxes
+    self.spec[:fields].collect { |f| f[:max] }
+  end
+
+  def next loc
+    max = self.maxes
+    parts = loc.split('.')
+    num_fields = parts.length
+    # increase least significant field
+    # if it is greater than max, set it to zero and increase the next field
+    # if there is no next field, then return nil
+  end
+
+  def min_empty_location
+    ""
   end
 
 end
