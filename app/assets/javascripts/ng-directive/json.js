63e7524d9ff1ddb553b8e879bf818cace3d07e91
Eric Klavins <klavins@uw.edu>
2016-05-20 07:46:14 -0700

    moved json directive to aquarium app

---
+++ app/assets/javascripts/ng-directive/json.js
@@ -0,0 +1,32 @@
+(function() {
+
+  var w;
+
+  try {
+    w = angular.module('aquarium'); 
+  } catch (e) {
+    w = angular.module('aquarium', ['ngCookies']); 
+  } 
+
+  w.directive('jsonText', function() {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function(scope, element, attr, ngModel) {            
+        function into(input) {
+          try {
+            return JSON.parse(input);
+          } catch(e) {
+            return {};
+          }
+        }
+        function out(data) {
+          return JSON.stringify(data,null,2);
+        }
+        ngModel.$parsers.push(into);
+        ngModel.$formatters.push(out);
+      }
+    };
+  });    
+
+})();
