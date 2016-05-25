c4e57349a0b350b0a0268361e5db24ac7a35520c
Eric Klavins <klavins@uw.edu>
2016-05-25 16:10:05 -0700

    can set and get array properties

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -131,6 +131,86 @@ class Sample < ActiveRecord::Base
 
   end
 
+  def set_property_aux ft, fv, val
+
+    case ft.ftype 
+
+    when 'string', 'url'
+      self.errors.add(:set_property,"#{val} is not a string") unless val.class == String
+      fv.value = val
+
+    when 'number'
+      self.errors.add(:set_property,"#{val} is not a number") unless val.respond_to? :to_f
+      fv.value = val.to_s
+  
+    when 'sample'
+      self.errors.add(:set_property,"#{val} is not a sample") unless val.class == Sample
+      fv.child_sample_id = val.id
+
+    when 'item'
+      self.errors.add(:set_property,"#{val} is not a item") unless val.class == Item
+      fv.child_item_id = val.id
+
+    end
+
+    fv
+
+  end
+
+  def set_property name, val 
+
+    ft = field_type name
+
+    unless ft
+      self.errors.add(:no_such_property,"Sample #{id} does not have a property named #{name}.")
+      return nil
+    end
+
+    fvs = field_values.select { |fv| fv.name == name }
+
+    if ft.array && val.class == Array
+
+      new_fvs = val.collect { |v|
+        set_property_aux(ft,field_values.create(name: name),v)
+      }
+
+      if self.errors.empty? 
+        new_fvs.each { |fv| fv.save }
+        fvs.each { |fv| fv.destroy }
+      end
+
+      return new_fvs
+
+    elsif ft.array && val.class != Array      
+
+      self.errors.add(:set_property,"Tried to set property #{ft.name}, an array, to something that is not an array.")
+      return nil
+
+    elsif !ft.array && val.class == Array
+
+      self.errors.add(:set_property,"Tried to set property #{ft.name}, which is not an array, to something is an array.")
+      return nil
+
+    elsif !ft.array && val.class != Array      
+
+      if fvs.length == 0
+        fvs = [ field_values.create(name: name) ]
+      end
+
+      if ft && fvs.length == 1
+        fv = set_property_aux(ft,fvs[0],val)
+      else 
+        self.errors.add(:set_property,"Could not set sample #{id} property #{name} to #{val}")
+        return nil
+      end
+
+      fv.save if self.errors.empty?
+      return fv
+
+    end
+
+  end
+
   #################################################################
   # Old methods for dealing with string valued fields
 
@@ -146,69 +226,50 @@ class Sample < ActiveRecord::Base
     return nil
   end
 
-  def set_property name, val 
+  def basic_value ft, fv
 
-    ft = field_type name
-    fvs = field_values.select { |fv| fv.name == name }
+    if fv.value
 
-    if fvs.length == 0
-      fvs = [ field_values.create(name: name) ]
-    end
+      ft = field_type fv.name 
 
-    if ft && fvs.length == 1
-
-      fv = fvs[0]
-
-      case field_type(name).ftype 
-     
-      when 'string', 'url'
-        raise "#{val} is not a string" unless val.class == String
-        fv.value = val
-
-      when 'number'
-        raise "#{val} is not a number" unless val.respond_to? :to_f
-        fv.value = val.to_s
-    
-      when 'sample'
-        raise "#{val} is not a sample" unless val.class == Sample
-        fv.child_sample_id = val.id
-
-      when 'item'
-        raise "#{val} is not a item" unless val.class == Item
-        fv.child_item_id = val.id
-
+      if ft.ftype == 'number'
+        fv.value.to_f
+      else
+        fv.value
       end
 
-      fv.save
+    elsif fv.child_sample_id
 
-    else 
+      fv.child_sample
 
-      self.errors.add(:set_property,"Could not set sample #{id} property #{name} to #{val}")
-      nil
+    elsif fv.child_item_id
 
-    end
+      fv.child_item
 
-    # TODO: allow user to set array fields too
+    end    
 
   end
 
   def properties
+
     p = {}
-    field_values.each do |fv|
-      if fv.value
-        ft = field_type fv.name 
-        if ft.ftype == 'number'
-          p[fv.name] = fv.value.to_f
-        else
-          p[fv.name] = fv.value
+
+    sample_type.field_types.each do |ft|
+
+     values = field_values.select { |fv| fv.name == ft.name }.collect { |fv| basic_value ft, fv }
+
+     if ft.array
+        p[ft.name] = values
+      else
+        if values.length == 1
+          p[ft.name] = values[0]
         end
-      elsif fv.child_sample_id
-        p[fv.name] = fv.child_sample
-      elsif fv.child_item_id
-        p[fv.name] = fv.child_item
       end
+
     end
+
     p
+
   end
 
   def value field_type
