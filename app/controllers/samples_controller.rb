0df7e31e6ed0b3e345129fcf3b5521de9cdcc565
Eric Klavins <klavins@uw.edu>
2016-07-21 20:40:26 -0700

    restored accidentally removed method

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -218,6 +218,25 @@ class SamplesController < ApplicationController
 
   end
 
+  def upgrade s, st
+
+    (1..8).each do |i|
+      n = st.fieldname i       
+      t = st.fieldtype i 
+      if t == [ 'string' ] || t == [ 'url' ] || t == [ 'number' ]
+        fv = s.field_values.new name: n, value: s["field#{i}"].to_s
+      elsif t != [ 'not used' ] && s["field#{i}"] && s["field#{i}"] != '-none-' && s["field#{i}"] != '' && s["field#{i}"] != 'NA'
+        c = Sample.find_by_name(s["field#{i}"])
+        @messages << "Sample #{s.id}: #{n}: Could not find '#{s["field#{i}"]}' with type in #{t}." unless c
+        fv = s.field_values.new(name: n, child_sample_id: c.id) if c
+      end
+      fv.save if fv
+    end
+
+    return s
+
+  end
+
   def process_spreadsheet
 
     if !params[:spreadsheet]
@@ -235,7 +254,7 @@ class SamplesController < ApplicationController
       if @samples.length > 0
         @samples.each do |s|
           s.save
-          SamplesHelper.upgrade(s, s.sample_type)
+          upgrade(s, s.sample_type)
         end
       end
 
