0d030bacb006c9bafd5b8f0d47e35aac1020508a
Eric Klavins <klavins@uw.edu>
2016-08-26 09:01:13 -0700

    a very unfortunate typo

--- app/accounting/cost_model.rb
+++ app/accounting/cost_model.rb
@@ -64,7 +64,7 @@ module CostModel
           labor: 9.9 * n * labor_rate
         }
 
-      when ["sequencing","send to sequencing"]
+      when ["sequencing","sent to sequencing"]
         n = 0
         if simple_spec[:num_colonies]
           m = simple_spec[:num_colonies].length
