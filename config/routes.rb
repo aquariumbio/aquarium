38b05d337b45bc6c21b9e379776283a3d13994de
Garrett <garrett.s.newman@gmail.com>
2017-01-14 17:55:51 -0800

    formatting

--- config/routes.rb
+++ config/routes.rb
@@ -180,13 +180,13 @@ Bioturk::Application.routes.draw do
 
   root to: 'static_pages#home'
 
-  match '/',        to: 'static_pages#home'
+  match '/',           to: 'static_pages#home'
 
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/signin',  to: 'sessions#new'
-  match '/signout', to: 'sessions#destroy', via: :delete
-  match '/status',      to: 'static_pages#status'
+  match '/help',       to: 'static_pages#help'
+  match '/about',      to: 'static_pages#about'
+  match '/signin',     to: 'sessions#new'
+  match '/signout',    to: 'sessions#destroy', via: :delete
+  match '/status',     to: 'static_pages#status'
   match '/analytics',  to: 'static_pages#analytics'
   match '/jobchart',   to: 'static_pages#jobchart'
   match '/location',   to: 'static_pages#location'
