5960fe584dfb67b5edc39ccc8244bb33a6d2b78e
Eric Klavins <klavins@uw.edu>
2017-04-20 10:52:15 -0700

    budget info loads more cleanly

--- app/assets/javascripts/models/budget.js
+++ app/assets/javascripts/models/budget.js
@@ -6,6 +6,7 @@ AQ.Budget.record_getters.spent = function() {
 
   AQ.get("/budgets/" + budget.id + "/spent").then(response => {
     budget.spent = response.data;
+    // AQ.update();
   })
 
   return budget.spent;
