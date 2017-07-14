d60ac7db77516136d7017c1c49f14e05c46c7de9
Eric Klavins <klavins@uw.edu>
2017-07-14 13:06:57 -0700

    fixed a bug where error and done operations are not listed in order after clicking "More" in manager

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -30,7 +30,7 @@ class JsonController < ApplicationController
         result = result.where(params[:arguments]) 
         result = result.limit(params[:options][:limit]) if params[:options] && params[:options][:limit] && params[:options][:limit].to_i > 0
         result = result.offset(params[:options][:offset]) if params[:options] && params[:options][:offset] && params[:options][:offset].to_i > 0
-        result = result.reverse if params[:options] && params[:options][:reverse]                
+        result = result.order('created_at DESC') if params[:options] && params[:options][:reverse]                
       end
 
       result = result.as_json(methods: params[:methods]) if ( params[:methods] )
