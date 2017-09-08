4a92bcfa0a9b6f9879e6e5f146c88c6adacd9b74
Eric Klavins <klavins@uw.edu>
2017-09-07 17:32:59 -0700

    inf loop detection in save test

--- app/assets/javascripts/models/test.js
+++ app/assets/javascripts/models/test.js
@@ -1,6 +1,5 @@
 AQ.Test = {};
 
-
 AQ.Test.plan_diff_aux = function (A,B,pkey) {
 
   // This method is used to recursively check that two plans are equal before and after saving.
@@ -11,21 +10,27 @@ AQ.Test.plan_diff_aux = function (A,B,pkey) {
     'next_module_id', 'inputs', 'outputs', 'current_module', "from_module", "to_module", "module_list"
   ];
 
-  for ( key in A ) {
+  if ( AQ.Test.num_calls < 100 ) {
 
-    if ( A[key] && A[key] != [] && !ignore.includes(key) && typeof A[key] != 'function' && ( B[key] === undefined  || B[key] === null ) ) {
+    for ( key in A ) {
 
-      console.log([A, A.rid, " defines " + key + " as ", A[key], " but ", B, B.rid, " does not"])
+      if ( A[key] && A[key] != [] && !ignore.includes(key) && typeof A[key] != 'function' && ( B[key] === undefined  || B[key] === null ) ) {
 
-    }  else if ( typeof A[key] == "object" && B[key] && !ignore.includes(key) ) {
+        console.log([A, A.rid, " defines " + key + " as ", A[key], " but ", B, B.rid, " does not"])
 
-      AQ.Test.plan_diff_aux(A[key], B[key],key)
+      }  else if ( typeof A[key] == "object" && B[key] && !ignore.includes(key) ) {
 
-    } else if ( typeof A[key] != 'function' && !ignore.includes(key) && A[key] != B[key] ) {
+        AQ.Test.plan_diff_aux(A[key], B[key],key)
 
-      console.log([key,A,B,A.rid, B.rid,key,A[key],B[key]])
+      } else if ( typeof A[key] != 'function' && !ignore.includes(key) && A[key] != B[key] ) {
+
+        console.log([key,A,B,A.rid, B.rid,key,A[key],B[key]])
+
+      }
 
     }
+
+    AQ.Test.num_calls++;
 
   }
 
@@ -34,6 +39,7 @@ AQ.Test.plan_diff_aux = function (A,B,pkey) {
 AQ.Test.plan_diff = function (A,B) {
 
   console.log("---- CHECKING FOR DIFFERENCES BETWEEN BEFORE AND AFTER SAVING---------------" );
+  AQ.Test.num_calls = 0;
   AQ.Test.plan_diff_aux(A,B,"plan");
   console.log("---- DONE CHECKING ---------------------------------------------------------" );
 
