5765c572664e6078cd3f82f52b45a0c977665b2e
Ben Keller <bjkeller@uw.edu>
2017-09-17 21:54:56 -0700

    Replace factory with run block

--- app/assets/javascripts/ng-helper/beforeunload_factory.js
+++ app/assets/javascripts/ng-helper/beforeunload_factory.js
@@ -5,24 +5,13 @@
     // Factory to broadcast `onbeforeunload` event to angular
     // see https://gist.github.com/981746/3b6050052ffafef0b4df
     //
-    w.factory('beforeUnload', function ($rootScope, $window) {
-        // Events are broadcast outside the Scope Lifecycle
+   w.run(['$rootScope', '$window', ($rootScope, $window) => {
+    $window.onbeforeunload = function (e) {
+      let event = $rootScope.$broadcast('onBeforeUnload');
+      if (event.defaultPrevented) {
+        return true;
+      }
+     }
+   }]);
 
-
-        // Listener should signal need for default dialog box by calling e.preventDefault()
-        $window.onbeforeunload = function (e) {
-            let event = $rootScope.$broadcast('onBeforeUnload');
-            if (event.defaultPrevented) {
-                return true;
-            }
-        };
-
-        $window.onunload = function () {
-            $rootScope.$broadcast('onUnload');
-        };
-        return {};
-    })
-        .run(function (beforeUnload) {
-            // Must invoke the service at least once
-        });
 })();
