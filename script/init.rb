5260a520d30e8fdd4adcd3e2ec2600bba0d8601b
Ben Keller <bjkeller@uw.edu>
2017-09-20 09:38:24 -0700

    Change init script so that adds user to admin group

--- script/init.rb
+++ script/init.rb
@@ -1,10 +1,9 @@
-def make_user name, login, password, opts={}
-
+def make_user(name, login, password, opts = {})
   options = { admin: false }. merge opts
 
   user = User.new(name: name, login: login, password: password, password_confirmation: password)
   user.admin = options[:admin]
-  user.save 
+  user.save
 
   group = Group.new
   group.name = user.login
@@ -16,6 +15,13 @@ def make_user name, login, password, opts={}
   m.user_id = user.id
   m.save
 
-  user
+  if user.admin
+    admin_id = Group.where(name: "admin").first.id
+    admin_membership = Membership.new
+    admin_membership.group_id = admin_id
+    admin_membership.user_id = user.id
+    admin_membership.save
+  end
 
+  user
 end
