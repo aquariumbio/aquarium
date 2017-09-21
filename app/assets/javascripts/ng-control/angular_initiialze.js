cddad725e33fb757931132593438fe5c3e257f6e
Ben Keller <bjkeller@uw.edu>
2017-09-21 07:14:05 -0700

    Change Angular HTTP configuration for CSRF cookie name

--- app/assets/javascripts/ng-control/angular_initiialze.js
+++ app/assets/javascripts/ng-control/angular_initiialze.js
@@ -2,18 +2,29 @@
 
   // Set up angular module for aquarium.
 
-  var w = angular.module('aquarium', 
+  let w = angular.module('aquarium',
           ['ngCookies','ui.ace','ngMaterial','ngMdIcons', 'ngAnimate'], 
-          [ '$rootScopeProvider', function($rootScopeProvider) { 
+          [ '$rootScopeProvider', '$httpProvider', function($rootScopeProvider, $httpProvider) {
       // This is an apparently well known hack that prevents digest errors when recursively
       // rendering templates that nest more than 10 levels.
-      $rootScopeProvider.digestTtl(25); 
+      $rootScopeProvider.digestTtl(25);
+
+      // Changes the CSRF cookie name used by Angular for front-end
+      // Requires corresponding change for back-end
+      $httpProvider.interceptors.push(function(){
+        return {
+          'request': function(config) {
+            config['xsrfCookieName'] = 'XSRF-TOKEN_' + aquarium_environment_name;
+            return config;
+          }
+        };
+      } )
     }]);
 
   w.filter('capitalize', function() {
       return function(input) {
         return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
       }
-  });  
+  });
 
 })();
