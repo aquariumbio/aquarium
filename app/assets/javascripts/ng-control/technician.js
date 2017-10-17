87abb6b00babee9c5027e9dc32d690f0ff4c7f0f
Eric Klavins <klavins@uw.edu>
2017-10-16 20:30:49 -0700

    basic rendering for steps

--- app/assets/javascripts/ng-control/technician.js
+++ app/assets/javascripts/ng-control/technician.js
@@ -2,8 +2,8 @@
 
   var w = angular.module('aquarium');
 
-  w.controller('technicianCtrl', [ '$scope', '$http', '$attrs', '$cookies',
-                        function (  $scope,   $http,   $attrs,   $cookies ) {
+  w.controller('technicianCtrl', [ '$scope', '$http', '$attrs', '$cookies', '$sce',
+                        function (  $scope,   $http,   $attrs,   $cookies,   $sce ) {
 
     AQ.init($http);
     AQ.update = () => { $scope.$apply(); }
@@ -19,6 +19,36 @@
 
     $scope.state = {
       index: 0
+    };
+
+    $scope.content_type = function(line) {
+      var type = Object.keys(line)[0];
+      return type;
+    };
+
+    $scope.content_value = function(line) {
+      var k = Object.keys(line)[0];
+      if ( typeof line[k] === "string" ) {
+        return $sce.trustAsHtml(line[k]);
+      } else {
+        return line[k];
+      }
+    };
+
+    $scope.table_class = function(cell) {
+      var c = "";
+      if ( cell == null ) {
+        c += " td-null-cell";
+      } else if ( cell.class ) {
+        c += cell.class;
+      }
+      if ( cell && cell.check ) {
+        c += " td-check"
+      }
+      if ( cell && cell.type ) {
+        c += " td-input"
+      }
+      return c;
     };
 
     $scope.keyDown = function(evt) {
