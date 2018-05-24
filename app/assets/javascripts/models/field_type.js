0c3b3bdfb491ae8b757faefa72f33a794615e46b
Eric Klavins <klavins@uw.edu>
2018-05-24 13:59:34 -0700

    show error when loading a plan with a field type pointing to a deleted allowable field type

--- app/assets/javascripts/models/field_type.js
+++ app/assets/javascripts/models/field_type.js
@@ -37,7 +37,10 @@ AQ.FieldType.record_methods.can_produce = function(fv) {
 
     aq.each(ft.allowable_field_types, (aft) => {
       if ( !fv.aft ) {
-        console.log("Warning. field value " + fv.name + " has no fv.aft.")
+        if (!fv.corrupt ) {
+          add_designer_message(`Warning. field value '${fv.name}' has no allowable types. This plan may have been corrupted due to altered i/o definitions.`);
+          fv.corrupt = true;
+        }
       } else if ( fv.aft.sample_type_id == aft.sample_type_id &&
          fv.aft.object_type_id == aft.object_type_id && 
          Number(fv.field_type.part) == Number(ft.part) ) { // Note, Number is used to compare null and false.
