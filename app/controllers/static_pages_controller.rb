56c62038dcfe130aa78a126e593c8c7ab683e723
Eric Klavins <klavins@uw.edu>
2018-05-31 20:21:40 -0700

    fixed layout for direct purchase

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -184,7 +184,7 @@ class StaticPagesController < ApplicationController
 
     plan = Plan.new(name: "Direct Purchase by " + current_user.name, budget_id: budgets[0].id)
     plan.save
-    op = dp.operations.create status: "pending", user_id: current_user.id, x: 100, y: 100, parent_id: -1
+    op = dp.operations.create status: "pending", user_id: current_user.id, x: 100, y: 100, parent_id: 0
     op.associate_plan plan
     job,operations = dp.schedule([op], current_user, Group.find_by_name(current_user.login))
 
