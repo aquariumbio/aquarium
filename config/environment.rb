e04141f95e4be0b09788bee53fe59857390f34c4
Abe Miller <abraham.r.m@gmail.com>
2018-07-05 14:18:43 -0700

    Add todo marker for Collection.count problem

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,8 @@ rescue Mysql2::Error => e
   puts "Initialization failed with #{e.class}, message: #{e.message}"
 end
 
-# This doesn't work in application.rb / after_initialize because the Collection constant name conflicts
+# TODO: This doesn't work in application.rb / after_initialize because the Collection constant name conflicts
 # with a rails class of the same name which we've never used (and didn't know about). The best thing to
 # do at some point would be to rename this class.
+
 # Collection.count
