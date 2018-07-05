afe26112c15ff8001ac73df89b6b00bca6b8ee81
Abe Miller <abraham.r.m@gmail.com>
2018-07-05 15:08:15 -0700

    Remove krill require statement in application.rb

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@
 require File.expand_path('boot', __dir__)
 
 require 'rails/all'
-require './lib/krill/krill'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
