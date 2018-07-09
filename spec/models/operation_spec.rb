0c98ca89de4afb3aa47ff074dc1d3fce8878e640
Abe Miller <abraham.r.m@gmail.com>
2018-07-09 12:06:42 -0700

    Add ShowResponse spectest

--- spec/models/operation_spec.rb
+++
@@ -1,7 +0,0 @@
-
-
-require 'rails_helper'
-
-RSpec.describe Operation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__.split('/').last}"
-end
--- spec/models/parameter_spec.rb
+++
@@ -1,7 +0,0 @@
-
-
-require 'rails_helper'
-
-RSpec.describe Parameter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
