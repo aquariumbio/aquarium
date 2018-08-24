f6b517204e4e21138f24792427f496dac8a8cb0b
Eric Klavins <klavins@uw.edu>
2018-08-24 10:41:22 -0700

    comments

--- ui/cypress/integration/designer/build_plan.js
+++ ui/cypress/integration/designer/build_plan.js
@@ -11,37 +11,43 @@ describe('Build plan', function() {
 
     // Go to designer page
     cy.login();  
-    cy.designer();   
+    cy.designer();
 
-    // Build a plan
+    // Choose Cloning
     cy.get('[data-sidebar=design]').click()
-    cy.get('[data-design=Operation_Types]').click()
+    cy.get("[data-design='Operation Types']").click()
     cy.get("[data-operation-type-category='Basic Cloning']").click()
 
+    // PCR
     cy.get("[data-add-operation-type='Make PCR Fragment']").click()
+    cy.get("#dismiss-messages").click()
 
     cy.get("[data-output-of='Make PCR Fragment'][data-output-number=0]").click()
+
+    // Run Gel
     cy.get("[data-successor='Run Gel']").click()
-
     cy.get("[data-input-of='Run Gel'][data-input-number=1]").click()
+
+    // Pour Gel
     cy.get("[data-predecessor='Pour Gel']").click()    
-
     cy.get("[data-input-of='Make PCR Fragment'][data-input-number=0]").click()
-    cy.get("[data-predecessor='Rehydrate Primer']").click()    
-    
-    cy.get("[data-input-of='Rehydrate Primer'][data-input-number=0]").click()
-    cy.get("[data-predecessor='Order Primer']").click()
 
+    // Rehydrate Primer
+    cy.get("[data-predecessor='Rehydrate Primer']").click()       
+    cy.get("[data-input-of='Rehydrate Primer'][data-input-number=0]").click()
+
+    // Order Primer
+    cy.get("[data-predecessor='Order Primer']").click()
     cy.get("[data-operation-box='Run Gel']").click()
+
+    // Fill in samples and parameters
     cy.get('[data-input-type=output][data-input-name=Fragment]').type("Sec")
     cy.contains('Second Fragment').click()
-
-    cy.wait(2000)
-
+    cy.wait(1000)
     cy.get("[data-operation-box='Order Primer']").click()
     cy.get("[data-parameter='Urgent?']").type("yes");
 
-    cy.get("[data-operation-box='Run Gel']").click()
+    cy.get("#plan-editor-container").scrollTo(0,0)
 
     // Give the plan a name, save it, and make sure it has an id
     cy.get("#plan-title-input").clear().type("My Test Plan")
@@ -49,6 +55,8 @@ describe('Build plan', function() {
     cy.wait(['@savePlan', '@getBudget', '@getFolders']).then((xhrs) => {
       cy.wrap(xhrs[0].response.body.id).should('be.above', 0)
     })
+
+    cy.get("[data-sidebar=launch]").click()
 
   });
 
