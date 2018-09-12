734589de87563c4b5052c425dc8d005a4a10564a
Ben Keller <bjkeller@uw.edu>
2018-09-12 08:11:06 -0700

    Add sessions spec

---
+++ spec/controllers/sessions_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+
+  remember_token_name = "remember_token_#{Bioturk::Application.environment_name}"
+
+  describe "Sessions" do
+
+    it "logs in and gets the remember_token cookie" do
+
+      post :create, { session: { login: "neptune", password: "aquarium" } }, as: 'html'
+      expect(response.cookies).to have_key(remember_token_name)
+    end
+
+  end
+
+end
+
+
+
