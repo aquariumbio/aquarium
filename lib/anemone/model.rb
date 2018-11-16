a4bf3e9a8a2e0ff986e75b6c8e2af27e305bfc97
Aquadoc <info@aquarium.com>
2018-11-16 11:11:59 -0800

    added backtrace to error

--- lib/anemone/model.rb
+++ lib/anemone/model.rb
@@ -25,10 +25,13 @@ module Anemone
 
           begin
             yield
-            rescue Exception => e
+          rescue Exception => e
             worker.status = "error"
             worker.message = e.to_s + ": " + e.backtrace.join(", ")
             worker.save
+            if worker.errors.any?
+              raise "Eror: Could not save worker #{worker.id} status"
+            end
           else
             worker.status = "done"
             worker.save
