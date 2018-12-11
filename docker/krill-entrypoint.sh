4f725e75f432719424b333b72619f3c5751857a5
Ben Keller <bjkeller@uw.edu>
2018-12-11 12:46:10 -0800

    Remove asset compilation from Krill script

--- docker/krill-entrypoint.sh
+++ docker/krill-entrypoint.sh
@@ -15,18 +15,8 @@ while ! nc -z db 3306; do
 done
 
 if [[ $1 == "development" || $1 == "production" ]]; then
-  # If container is run without exactly 2 arguments then start server
-
-  if [[ $1 == "production" ]]; then
-    # production server must have assets precompiled 
-    # ALSO SEE application.rb lines 8:13
-    echo "precompiling assets"
-    exec bundle exec rake assets:precompile
-  fi
-
   echo "Starting Krill runner"
   exec bundle exec rails runner -e $1 'Krill::Server.new.run(3500)'
-
 else
   # If the normal image startup flags were not given as arguments, 
   # then exec whatever arguments were given
