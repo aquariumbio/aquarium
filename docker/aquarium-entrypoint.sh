346bb9ce4fa119aa662cf607381a02f734b2f9c6
Ben Keller <bjkeller@uw.edu>
2018-12-27 13:42:02 -0800

    Fix aquarium entrypoint so that only exec puma

--- docker/aquarium-entrypoint.sh
+++ docker/aquarium-entrypoint.sh
@@ -21,8 +21,10 @@ elif [[ $1 == "production" ]]; then
   # Production server must have assets precompiled
   # Note only works once db is up (e.g., can't be done in Dockerfile)
   # ALSO SEE application.rb lines 8:13
+  # TODO: figure out why precompiling requires database and must be done here
   echo "Precompiling assets"
-  exec bundle exec rake assets:precompile
+  RAILS_ENV=production
+  bundle exec rake assets:precompile
   echo "Starting Rails server"
   exec bundle exec puma -C config/production_puma.rb -e $1
 else
