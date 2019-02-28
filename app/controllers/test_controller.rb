3b8b245c3a55bfdfe558731d0689dd3db0a9089c
Ben Keller <bjkeller@uw.edu>
2019-02-28 15:44:12 -0800

    Refined error handling
    Refined error handling:
    1. get protocol errors from job since krill eats exceptions.
    2. Catch assertion exceptions on analyze.
    3. Catch other exceptsions from both setup and analyze as test errors.

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -14,26 +14,34 @@ class TestController < ApplicationController
     ActiveRecord::Base.transaction do
 
       begin
-      eval(code)
+        eval(code)
         pt = ProtocolTest.new ot, current_user
+
         pt.setup
         pt.run
 
-        if pt.error
-          resp.error(pt.error)
-              .more(backtrace: pt.backtrace, log: pt.logs)
+        if pt.job.error?
+          resp.error("Protocol error: #{pt.job.error_message}")
+              .more(backtrace: pt.job.error_backtrace, log: [])
         else
           begin
             pt.analyze
-          rescue Exception => e
+          rescue Minitest::Assertion => e
             resp.error("Test failed: #{e.to_s}")
-                .more(backtrace: pt.backtrace, log: pt.logs)
+                .more(
+                  exception_backtrace: e ? e.backtrace : [], 
+                  backtrace: pt ? pt.backtrace : [], 
+                  log: pt ? pt.logs : [])
           else
             resp.ok(message: "test complete", 
                         log: pt.logs, 
                   backtrace: pt.backtrace)
           end
         end
+      rescue StandardError => error
+        resp.error("Test error: #{error.to_s}")
+            .more(exception_backtrace: error.backtrace,
+                  backtrace: [], log: [])
       rescue Exception => e
         resp.error(e.to_s)
             .more(
