bf30e0c29a372c627e83827a276e8042c7dc3d8b
Eric Klavins <klavins@uw.edu>
2019-03-04 12:31:58 -0800

    updated release version

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,3 +1,3 @@
-Bioturk::Application.config.aquarium_version = "2.6.0-alpha"
+Bioturk::Application.config.aquarium_version = "2.6.0"
 
 puts "Aquarium Version #{Bioturk::Application.config.aquarium_version} Starting!"
