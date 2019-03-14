22e0d2bdf35740de8148da5e9cf49d47596d3bd2
Ben Keller <bjkeller@uw.edu>
2019-03-14 13:38:35 -0700

    Update publish controller
    Update publish controller:
    1. Change handlers to catch StandardError.
    2. Remove extra blank lines.

--- app/controllers/publish_controller.rb
+++ app/controllers/publish_controller.rb
@@ -8,7 +8,6 @@ class PublishController < ApplicationController
   end
 
   def check_repo # This should be moved to aquadoc
-
     resp = AqResponse.new
 
     github_user = params[:organization] || params[:user]
@@ -20,8 +19,8 @@ class PublishController < ApplicationController
         client = Octokit::Client.new(:access_token => params[:access_token])
         logger.info "Rate Limit Info: #{client.rate_limit}"
         repos = client.repositories(github_user).collect { |r| r.name }
-        logger.info repos
-      rescue Exception => e
+        logger.info(repos)
+      rescue StandardError => e
         logger.info(e)
         resp.error("Aquarium cannot access Github using the supplied access token.", e)
       else
@@ -31,7 +30,7 @@ class PublishController < ApplicationController
             config = JSON.parse Base64.decode64(file[:content])
             file = client.contents({ repo: params[:repo], user: github_user}, path: "/#{params[:repo]}.aq")
             aq_file = JSON.parse Base64.decode64(file[:content])            
-          rescue Exception => e
+          rescue StandardError => e
             resp.error("The Github repository '#{params[:repo]}' exists but does not contain a config.json file.", e)
           else
             resp.ok(repo_exists: true, config: config, aq_file: aq_file)
@@ -43,7 +42,6 @@ class PublishController < ApplicationController
     end
 
     render json: resp
-
   end
 
   def categories
@@ -60,51 +58,33 @@ class PublishController < ApplicationController
   end
 
   def publish
-
     resp = AqResponse.new
-
     if params[:categories]
-
-      worker = Anemone::Worker.new name: "publisher"
+      worker = Anemone::Worker.new(name: "publisher")
       worker.save
 
       worker.run do
-
         ag = Aquadoc::Git.new(params[:config], categories)
         ag.run
-
       end
 
-      resp.ok worker_id: worker.id
-
+      resp.ok(worker_id: worker.id)
     else
-
       resp.error("No operation types selected.")
-
     end
 
     render json: resp
-
   end
 
   def export
-
     resp = AqResponse.new    
-
     if params[:categories]
-
       ar = Aquadoc::Render.new(nil, params[:config], categories)
       resp.ok(aq_file: ar.aq_file)
-
     else
-
       resp.error("No operation types selected.")      
-
     end    
 
     render json: resp
-
   end
-
-
 end
