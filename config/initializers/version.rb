6601b2a1d3937f5a4c14a12a3d2e8581b7e3525d
Ben Keller <bjkeller@uw.edu>
2019-03-20 13:20:41 -0700

    Set aquarium version to 2.5.2

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,3 +1,3 @@
-Bioturk::Application.config.aquarium_version = "2.5.1"
+Bioturk::Application.config.aquarium_version = "2.5.2"
 
 puts "Aquarium Version #{Bioturk::Application.config.aquarium_version} Starting!"
