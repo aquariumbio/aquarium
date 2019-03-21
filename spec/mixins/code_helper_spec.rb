d75d57ce91543328aa9b4c3c2169038cbfbd3d2e
Ben Keller <bjkeller@uw.edu>
2019-03-21 11:50:57 -0700

    Add tests for code and code_helper

---
+++ spec/mixins/code_helper_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe CodeHelper do
+  let(:mock_owner_class) { build_mock_class }
+  let!(:a_user) { create(:user)}
+
+  before(:all) { create_table }
+  after(:all) { drop_table }
+
+  before(:each) do
+    @owner = mock_owner_class.new
+    @owner.save
+  end
+  
+  it 'has no code if not added' do
+    expect(@owner.code).to be_nil
+    expect(@owner.code('the_code')).to be_nil
+  end
+
+  it 'has code if added' do
+    code_object = @owner.new_code('the_code', 'def noop; end', a_user)
+    expect(code_object).not_to be_nil
+    expect(@owner.code('the_code')).not_to be_nil
+  end
+
+  it 'cannot add code with same name' do
+    code_object = @owner.new_code('the_code', 'def noop; end', a_user)
+    expect { @owner.new_code('the_code', 'def noop; end', a_user) }.to raise_error
+  end
+
+  it 'attributes of created object are as expected' do
+    code_object = @owner.new_code('the_code', 'def noop; end', a_user)
+    expect(code_object.name).to eq('the_code')
+    expect(code_object.content).to eq('def noop; end')
+    expect(code_object.parent_class).to eq("#{mock_owner_class}")
+    expect(code_object.user_id).to eq(a_user.id)
+  end
+end
+
+def build_mock_class
+  Class.new(ActiveRecord::Base) do
+    self.table_name = 'mock_owner_table'
+    include CodeHelper
+  end
+end
+
+def create_table
+  ActiveRecord::Base.connection.create_table :mock_owner_table do |t|
+  end
+end
+
+def drop_table
+  ActiveRecord::Base.connection.drop_table :mock_owner_table
+end
---
+++ spec/models/code_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe CodeHelper do
+  let(:code) { create(:code) }
+  let(:a_user) { create(:user)}
+  
+  it 'example object has values from factory' do
+    expect(code.name).to eq('the_code')
+    expect(code.content).to eq('def the_code; end')
+    expect(code.parent_class).to eq('DummyClass')
+  end
+
+  it 'code should update after commit' do
+    expect(code.content).to eq('def the_code; end')
+    new_code = code.commit('def updated_code; end', a_user)
+    expect(new_code.content).to eq('def updated_code; end')
+  end
+
+  it 'versions should include all committed versions' do
+    expect(code.content).to eq('def the_code; end')
+    code2 = code.commit('def updated_code2; end', a_user)
+    code3 = code2.commit('def updated_code3; end', a_user)
+    expect(code.versions).to include(code, code2, code3)
+    expect(code2.versions).to include(code, code2, code3)
+  end
+end
