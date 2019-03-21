780bb0eb499946e131e40485b4c352f1b7b5c793
Ben Keller <bjkeller@uw.edu>
2019-03-21 11:50:21 -0700

    Add factory for code object

---
+++ spec/factories/code.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :code do
+    name { 'the_code' }
+    content { 'def the_code; end' }
+    parent_id { 1 }
+    parent_class { 'DummyClass' }
+    user_id { 1 }
+  end
+end
