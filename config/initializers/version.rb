d38792291761a280f88a2633155d9700f2dd2b99
Ben Keller <bjkeller@uw.edu>
2019-05-17 14:40:08 -0700

    Update docs

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,3 +1,3 @@
-Bioturk::Application.config.aquarium_version = "2.6.2"
+Bioturk::Application.config.aquarium_version = "2.6.3"
 
 puts "Aquarium Version #{Bioturk::Application.config.aquarium_version} Starting!"
