e1695dd21e13aa4f5fa91ade318a2cba24efa707
Ben Keller <bjkeller@uw.edu>
2019-05-22 12:49:02 -0700

    Add parentheses to nested call

--- lib/krill/base.rb
+++ lib/krill/base.rb
@@ -57,7 +57,7 @@ module Krill
       else
 
         # figure out default technician response
-        i = ShowResponse.new(simulated_input_for page)
+        i = ShowResponse.new(simulated_input_for(page))
         @job.append_step operation: 'next', time: Time.now, inputs: i
 
         raise "Job #{jid} executed too many steps (50) in debug mode. Could be an infinite loop." if @job.pc > 500
