522b47414ee69cc9f27e0864c5ce447451546288
Ben Keller <bjkeller@uw.edu>
2019-06-11 14:52:23 -0700

    Add transients for code to operation type factory

--- spec/factories/operation_type.rb
+++ spec/factories/operation_type.rb
@@ -2,9 +2,24 @@
 
 FactoryBot.define do
   factory :operation_type do
+    transient do
+      protocol {}
+      cost_model { 'def cost(_op); { labor: 0, materials: 0 } end' }
+      precondition { 'def precondition(_op); true end' }
+      user {}
+    end
+
     sequence(:name) { |n| "operation_type_#{n}" }
     category { 'the category' }
     deployed { true }
     initialize_with { OperationType.where(name: name, category: category).first_or_create }
+
+    after(:create) do |operation_type, evaluator|
+      if evaluator.protocol && evaluator.user
+        operation_type.add_protocol(content: evaluator.protocol, user: evaluator.user) 
+        operation_type.add_cost_model(content: evaluator.cost_model, user: evaluator.user)
+        operation_type.add_precondition(content: evaluator.precondition, user: evaluator.user)
+      end
+    end
   end
 end
