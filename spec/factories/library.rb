d2a77a96ef776cbd41f76e514e2c8ab4faf6ef80
Ben Keller <bjkeller@uw.edu>
2019-06-11 14:52:01 -0700

    Add transients for code to library factory

--- spec/factories/library.rb
+++ spec/factories/library.rb
@@ -2,7 +2,18 @@
 
 FactoryBot.define do
   factory :library do
+    transient do
+      source {}
+      user {}
+    end
+
     name { 'the_library' }
     category { 'the_test_category' }
+
+    after(:create) do |operation_type, evaluator|
+      if evaluator.source && evaluator.user
+        operation_type.add_source(content: evaluator.source, user: evaluator.user)
+      end
+    end
   end
 end
