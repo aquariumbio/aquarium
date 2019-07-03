bf27086a7437fad046f3f5109dd3151e4a9397a6
Ben Keller <bjkeller@uw.edu>
2019-07-03 14:08:20 -0700

    Remove skip

--- spec/controllers/test_operation_types_spec.rb
+++ spec/controllers/test_operation_types_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 
 RSpec.describe OperationTypesController, type: :controller do
 
-  #before { skip('needs to be rewritten b/c relies on samples existing for all operation types in the database') }
   let!(:test_user) { create(:user) }
   let(:dummy_sample_type) { create(:sample_type, name: 'DummySampleType') }
   let(:dummy_sample) { create(:sample, name: 'DummySample', sample_type: dummy_sample_type) }
@@ -27,56 +26,56 @@ RSpec.describe OperationTypesController, type: :controller do
 
   describe 'Tests operation types' do
 
-      it "test for correct protocol completes without error" do
-        cookies[token_name] = User.find(1).remember_token
+    it "test for correct protocol completes without error" do
+      cookies[token_name] = User.find(1).remember_token
 
-        # this shouldn't be necessary, but item isn't saved if not touched.
-        dummy_item.save
+      # this shouldn't be necessary, but item isn't saved if not touched.
+      dummy_item.save
 
-        get :random, id: io_protocol.id, num: 3
+      get :random, id: io_protocol.id, num: 3
 
-        post_data = io_protocol.as_json
-        post_data[:test_operations] = JSON.parse(@response.body)
-        post :test, post_data
+      post_data = io_protocol.as_json
+      post_data[:test_operations] = JSON.parse(@response.body)
+      post :test, post_data
 
-        response_hash = JSON.parse(@response.body, symbolize_names: true)
+      response_hash = JSON.parse(@response.body, symbolize_names: true)
 
-        expect(response_hash[:error]).to be_nil
-        puts response_hash[:error]
-        expect(response_hash[:operations]).not_to be_nil
-        response_hash[:operations].each do |op|
-          assert_equal 'done', op[:status]
-        end
+      expect(response_hash[:error]).to be_nil
+      puts response_hash[:error]
+      expect(response_hash[:operations]).not_to be_nil
+      response_hash[:operations].each do |op|
+        assert_equal 'done', op[:status]
       end
+    end
 
-        # A protocol with some funky syntax
-      let(:bad_syntax_protocol) do
-        create(
-          :operation_type,
-          name: 'bad_syntax',
-          category: 'testing',
-          protocol: 'class Protocol; def main; 1=2; end; end',
-          user: test_user
-        )
+    # A protocol with some funky syntax
+    let(:bad_syntax_protocol) do
+      create(
+        :operation_type,
+        name: 'bad_syntax',
+        category: 'testing',
+        protocol: 'class Protocol; def main; 1=2; end; end',
+        user: test_user
+      )
+    end
+
+    it "test for protocol with syntax error has error" do
+      cookies[token_name] = User.find(1).remember_token
+      get :random, id: bad_syntax_protocol.id, num: 3
+
+      post_data = bad_syntax_protocol.as_json
+      post_data[:test_operations] = JSON.parse(@response.body)
+      post :test, post_data
+
+      response_hash = JSON.parse(@response.body, symbolize_names: true)
+
+      expect(response_hash[:error]).to be_nil
+      puts response_hash[:error]
+      expect(response_hash[:operations]).not_to be_nil
+      response_hash[:operations].each do |op|
+        assert_equal 'done', op[:status]
       end
-
-      it "test for protocol with syntax error has error" do
-        cookies[token_name] = User.find(1).remember_token
-        get :random, id: bad_syntax_protocol.id, num: 3
-
-        post_data = bad_syntax_protocol.as_json
-        post_data[:test_operations] = JSON.parse(@response.body)
-        post :test, post_data
-
-        response_hash = JSON.parse(@response.body, symbolize_names: true)
-
-        expect(response_hash[:error]).to be_nil
-        puts response_hash[:error]
-        expect(response_hash[:operations]).not_to be_nil
-        response_hash[:operations].each do |op|
-          assert_equal 'done', op[:status]
-        end
-      end
+    end
 
   end
 end
