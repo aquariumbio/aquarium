3f13fa48ade882bfd0a16db9c82ea4d1e19920ab
Ben Keller <bjkeller@uw.edu>
2019-07-09 09:07:33 -0700

    Move test controller methods into test engine

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -42,18 +42,22 @@ class TestController < ApplicationController
                 backtrace: test ? test.backtrace : [],
                 log: test ? test.logs : []
               )
-      render json: response
-      return
     rescue Krill::KrillSyntaxError => e
       response.error(e.message)
               .more(
-                error_type: 'test_error',
+                error_type: 'syntax_error',
                 exception_backtrace: e.backtrace,
                 backtrace: [], log: []
               )
-      render json: response
-      return
     rescue Krill::KrillError => e
+      response.error(e.message)
+              .more(
+                error_type: 'protocol_error',
+                exception_backtrace: e.backtrace,
+                backtrace: test ? test.backtrace : [],
+                log: test ? test.logs : []
+              )
+    rescue KrillTestError => e
       response.error(e.message)
               .more(
                 error_type: 'test_error',
@@ -61,24 +65,9 @@ class TestController < ApplicationController
                 backtrace: test ? test.backtrace : [],
                 log: test ? test.logs : []
               )
-      render json: response
-      return
-    rescue KrillTestError => e
-      response.error(e.message)
-              .more(
-                error_type: 'test_error',
-                exception_backtrace: e.backtrace,
-                backtrace: test.backtrace, log: test.logs
-              )
-      render json: response
-      return
     end
 
-    if test
-      response.ok(message: 'test complete',
-                  log: test.logs,
-                  backtrace: test.backtrace)
-    elsif response.nil?
+    if response.nil?
       response = AqResponse.new.error(
         'Internal error: test completed with no response'
       )
@@ -87,145 +76,4 @@ class TestController < ApplicationController
     render json: response
   end
 
-  # Loads the test code.
-  #
-  # @param code [Code] the test code object
-  # @return [AqResponse] if an exception occurs, and `nil` otherwise
-  def load_test(code:)
-    begin
-      resp = nil
-      code.load(binding: empty_binding, source_name: '(eval)')
-    rescue ScriptError, StandardError => e
-      resp = handle_error(error: e, phase_name: 'test loading')
-    end
-
-    resp
-  end
-
-  # Runs the test `setup` method.
-  #
-  # @param test [ProtocolTest] the test object
-  # @return [AqResponse] if an exception occurs, and `nil` otherwise
-  def pre_test(test:)
-    begin
-      resp = nil
-      test.setup
-      unless test.operations_present?
-        resp = AqResponse.new
-        resp.error('Test error: setup must add operations')
-      end
-    rescue SystemStackError, ScriptError, StandardError => e
-      resp = handle_error(error: e, logs: test.logs)
-    end
-
-    resp
-  end
-
-  # Runs the protocol under test.
-  #
-  # @param test [ProtocolTest] the test object
-  # @return [AqResponse] if an exception occurs, and `nil` otherwise
-  def run_test(test:)
-    begin
-      resp = nil
-      test.run
-      if test.error?
-        resp = AqResponse.new
-        resp.error("Protocol error: #{test.error_message}")
-            .more(exception_backtrace: test.error_backtrace,
-                  log: test.logs)
-      end
-    rescue SystemStackError, ScriptError, StandardError => e
-      resp = handle_error(error: e, logs: test.logs)
-    end
-
-    resp
-  end
-
-  # Runs the test `analyze` method.
-  #
-  # @param test [ProtocolTest] the test object
-  # @return [AqResponse] if the protocol has an error, and `nil` otherwise
-  def post_test(test:)
-    begin
-      resp = AqResponse.new
-      test.analyze
-    rescue Minitest::Assertion => e
-      error_trace = filter_backtrace(backtrace: e.backtrace)
-                    .map { |message| translate_trace(message: message) }
-      resp.error("Assertion failed: #{e}")
-          .more(
-            error_type: 'assertion_failure',
-            exception_backtrace: error_trace,
-            backtrace: test ? test.backtrace : [],
-            log: test ? test.logs : []
-          )
-    rescue SystemStackError, ScriptError, StandardError => e
-      resp = handle_error(error: e)
-    else
-      resp.ok(message: 'test complete',
-              log: test.logs,
-              backtrace: test.backtrace)
-    end
-
-    resp
-  end
-
-  # Creates an `AqResponse` object for an error that occur in a test.
-  # Filters backtrace to include errors that occur within an `(eval)`.
-  #
-  # @param error [StandardError, ScriptError, SystemStackError] the exception object
-  # @return [AqResponse] object with error message, and backtrace.
-  def handle_error(error:, phase_name: nil, logs: [])
-    resp = AqResponse.new
-    error_trace = filter_backtrace(backtrace: error.backtrace)
-                  .map { |message| translate_trace(message: message) }
-    error_message = if error_trace[-1].present?
-                      "Error in #{error_trace[-1]}: #{error}"
-                    elsif error.is_a?(ScriptError) && error.message.match(/^\(eval\):\d+: .+$/)
-                      line_number, message = error.message.match(
-                        /^\(eval\):(\d+): (.+)$/
-                      ).captures
-                      "Test line #{line_number}: #{message}"
-                    elsif phase_name.present?
-                      "Error during #{phase_name}: #{error.message}"
-                    else
-                      "Error #{error.class} #{error.message}"
-                    end
-    resp.error(error_message)
-        .more(
-          error_type: 'test_error',
-          exception_backtrace: error_trace,
-          backtrace: [], log: logs
-        )
-
-    resp
-  end
-
-  # Filters the given backtrace for error messages that occur within an `(eval)`.
-  #
-  # @param backtrace [Array<String>] the backtrace to filter
-  # @return [Array<String>] the filtered backtrace
-  def filter_backtrace(backtrace:)
-    backtrace.reject { |msg| msg.match(/^\(eval\):\d+:in `.+'$/).nil? }.uniq
-  end
-
-  # Translates an error message of the form
-  #   (eval):10:in `method'
-  # to a message with just the method name and line number.
-  #
-  # @param message [String] the error message matching `eval` error pattern
-  # @return [String] object containing the method name and line number
-  def translate_trace(message:)
-    line_number, method = message.match(/^\(eval\):(\d+):in `(.+)'$/).captures
-
-    "`#{method}` (line #{line_number})"
-  end
-
-  private
-
-  # Create an empty binding
-  def empty_binding
-    binding
-  end
 end
--- app/models/protocol_test_engine.rb
+++ app/models/protocol_test_engine.rb
@@ -40,7 +40,7 @@ class ProtocolTestEngine
   end
 
   def load
-    manager = Krill::Manager.new(@environment.job, true)
+    manager = Krill::DebugManager.new(@environment.job)
     @environment.operations.make(role: 'input')
   end
 
@@ -58,69 +58,108 @@ class ProtocolTestEngine
   #### code based on TestController
 
   def load_job(job:)
-    manager = Krill::Manager.new(job, true)
+    manager = Krill::DebugManager.new(job)
     job.operations.make(role: 'input')
 
     manager
-  rescue  Krill::KrillSyntaxError => e
-    # TODO: mangle error for response
-    raise KrillTestError(e)
-  rescue StandardError => e
-    # TODO: decide whether to mangle error
-    raise KrillTestError(e)
   end
 
   # Loads the test code.
   #
   # @param code [Code] the test code object
-  def load_test(code:)
+  def self.load_test(operation_type:, user:)
     # TODO: this should be sandboxed
-    code.load(binding: empty_binding, source_name: '(eval)')
-    ProtocolTest.new(operation_type, current_user)
+    operation_type.test.load(binding: empty_binding, source_name: '(eval)')
+    ProtocolTest.new(operation_type, user)
   rescue ScriptError, StandardError => e
     # TODO: revisit
-    raise KrillTestError.new(e)
+    raise KrillTestError.new(message: "Error while loading test: #{e.message}", error: e)
   end
 
   # Runs the test `setup` method.
   #
   # @param test [ProtocolTest] the test object
-  def pre_test(test:)
+  def self.pre_test(test:)
     begin
       test.setup
-    rescue SystemStackError, ScriptError, StandardError => e
-      raise KrillTestError(e)
+    rescue ScriptError => e
+      message = if e.message.match(/^\(eval\):\d+: .+$/)
+                  line_number, message = e.message.match(
+                    /^\(eval\):(\d+): (.+)$/
+                  ).captures
+                  "Test line #{line_number}: #{message}"
+                else
+                  "Error during setup: #{e.class} #{e.message}"
+                end
+      raise KrillTestError.new(
+        message: message,
+        error_type: 'test_error',
+        error: e
+      )
+    rescue SystemStackError, StandardError => e
+      raise KrillTestError.new(message: 'Error in setup', error: e)
     end
-    unless test.operations_present?
-      raise KrillTestError(message: 'No operations after test setup')
-    end
+    raise KrillTestError.new(message: 'No operations after test setup') unless test.operations_present?
   end
 
   # Runs the protocol under test.
   #
   # @param test [ProtocolTest] the test object
-  def run_test(test:)
+  def self.run_test(test:)
     test.run
-  rescue SystemStackError, ScriptError, StandardError => e
-    raise KrillTestError(e)
+  rescue ScriptError => e
+    message = if e.message.match(/^\(eval\):\d+: .+$/)
+                line_number, message = e.message.match(
+                  /^\(eval\):(\d+): (.+)$/
+                ).captures
+                "Test line #{line_number}: #{message}"
+              else
+                "Error while running protocol: #{e.class} #{e.message}"
+              end
+    raise KrillTestError.new(
+      message: message,
+      error_type: 'test_error',
+      error: e
+    )
+  rescue Krill::KrillError => e
+    raise e
+  rescue SystemStackError, StandardError => e
+    raise KrillTestError.new(message: 'Error running protocol', error: e)
   end
 
   # Runs the test `analyze` method.
   #
   # @param test [ProtocolTest] the test object
-  def post_test(test:)
+  def self.post_test(test:)
     test.analyze
   rescue Minitest::Analyze => e
     raise e
-  rescue SystemStackError, ScriptError, StandardError => e
+  rescue ScriptError => e
+    message = if e.message.match(/^\(eval\):\d+: .+$/)
+                line_number, message = e.message.match(
+                  /^\(eval\):(\d+): (.+)$/
+                ).captures
+                "Test line #{line_number}: #{message}"
+              else
+                "Error during analysis: #{e.class} #{e.message}"
+              end
+    raise KrillTestError.new(
+      message: message,
+      error_type: 'test_error',
+      error: e
+    )
+  rescue SystemStackError, StandardError => e
     raise KrillTestError(e)
   end
 
-  def self.run(operation_type:)
+  # Run the test for the given operation type.
+  #
+  # @param operation_type [OperationType] the operation type
+  # @return [ProtocolTestBase] the execution object for the test
+  def self.run(operation_type:, user:)
     test = nil
-
     ActiveRecord::Base.transaction do
-      test = load_test(code: operation_type.test)
+      test = load_test(operation_type: operation_type, user: user)
       pre_test(test: test)
       run_test(test: test)
       post_test(test: test)
@@ -130,10 +169,72 @@ class ProtocolTestEngine
     test
   end
 
+  private
+
+  # TODO: decide what to do with rest
+
+  # Creates an `AqResponse` object for an error that occur in a test.
+  # Filters backtrace to include errors that occur within an `(eval)`.
+  #
+  # @param error [StandardError, ScriptError, SystemStackError] the exception object
+  # @return [AqResponse] object with error message, and backtrace.
+  def handle_error(error:, phase_name: nil, logs: [])
+    resp = AqResponse.new
+    error_trace = filter_backtrace(backtrace: error.backtrace)
+                  .map { |message| translate_trace(message: message) }
+    error_message = if error_trace[-1].present?
+                      "Error in #{error_trace[-1]}: #{error}"
+                    elsif error.is_a?(ScriptError) && error.message.match(/^\(eval\):\d+: .+$/)
+                      line_number, message = error.message.match(
+                        /^\(eval\):(\d+): (.+)$/
+                      ).captures
+                      "Test line #{line_number}: #{message}"
+                    elsif phase_name.present?
+                      "Error during #{phase_name}: #{error.message}"
+                    else
+                      "Error #{error.class} #{error.message}"
+                    end
+    resp.error(error_message)
+        .more(
+          error_type: 'test_error',
+          exception_backtrace: error_trace,
+          backtrace: [], log: logs
+        )
+
+    resp
+  end
+
+  # Filters the given backtrace for error messages that occur within an `(eval)`.
+  #
+  # @param backtrace [Array<String>] the backtrace to filter
+  # @return [Array<String>] the filtered backtrace
+  def filter_backtrace(backtrace:)
+    backtrace.reject { |msg| msg.match(/^\(eval\):\d+:in `.+'$/).nil? }.uniq
+  end
+
+  # Translates an error message of the form
+  #   (eval):10:in `method'
+  # to a message with just the method name and line number.
+  #
+  # @param message [String] the error message matching `eval` error pattern
+  # @return [String] object containing the method name and line number
+  def translate_trace(message:)
+    line_number, method = message.match(/^\(eval\):(\d+):in `(.+)'$/).captures
+
+    "`#{method}` (line #{line_number})"
+  end
+
+  # Create an empty binding
+  def self.empty_binding
+    binding
+  end
 end
 
 class KrillTestError < StandardError
-  def initialize(message:)
+  attr_reader :error
+
+  def initialize(message:, error:)
+    @error = error
     super(message)
   end
 end
