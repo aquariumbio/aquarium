c3c5c8b3ca6fa54cabe0d767af3228042e7bf092
Ben Keller <bjkeller@uw.edu>
2019-08-13 15:15:28 -0700

    Use field type type predicates

--- app/planner/operation_type_random.rb
+++ app/planner/operation_type_random.rb
@@ -15,13 +15,13 @@ module OperationTypeRandom
       samples = {}
 
       field_types.each do |field_type|
-        if field_type.ftype == 'sample'
+        if field_type.sample?
           if samples[field_type.routing]
             aft = field_type.choose_aft_for(samples[field_type.routing])
             field_sample = if !field_type.array
                              [samples[field_type.routing]].flatten.first
                            else
-                             [samples[field_type.routing]] * 3
+                             [samples[field_type.routing]] * ((rand()*10).floor + 1)
                            end
           else
             field_sample, aft = field_type.random
@@ -32,9 +32,9 @@ module OperationTypeRandom
           Rails.logger.info "Error adding property: #{op.errors.full_messages.join(', ')}" unless op.errors.empty?
         elsif field_type.choices != '' && !field_type.choices.nil?
           op.set_property(field_type.name, field_type.choices.split(',').sample, field_type.role, true, nil)
-        elsif field_type.type == 'number'
+        elsif field_type.number?
           op.set_property(field_type.name, rand(100), field_type.role, true, nil)
-        elsif field_type.ftype == 'json'
+        elsif field_type.json?
           op.set_property(field_type.name, '{ "message": "random json parameters are hard to generate" }', field_type.role, true, nil)
         else
           op.set_property(field_type.name, %w[Lorem ipsum dolor sit amet consectetur adipiscing elit].sample, field_type.role, true, nil)
