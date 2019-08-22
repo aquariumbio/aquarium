5620cdbf219cc84145cb65472fd687d29923d93c
Ben Keller <bjkeller@uw.edu>
2019-08-22 11:00:33 -0700

    Add exception details to method comments

--- app/models/protocol_test_engine.rb
+++ app/models/protocol_test_engine.rb
@@ -8,6 +8,8 @@ class ProtocolTestEngine
   # Loads the test code.
   #
   # @param code [Code] the test code object
+  # @raise [KrillTestSyntaxError] if load raises {SyntaxError}
+  # @raise [KrillTestError] if other execution errors occur
   def self.load_test(operation_type:, user:)
     # TODO: this should be sandboxed
     operation_type.test.load(binding: empty_binding, source_name: '(eval)')
@@ -22,6 +24,9 @@ class ProtocolTestEngine
   #
   # @param operation_type [OperationType] the operation type
   # @return [ProtocolTestBase] the execution object for the test
+  # @raise [KrillTestSyntaxError] if a SyntaxError occurs during execution
+  # @raise [KrillTestError] if another error occurs during test execution
+  # @raise [KrillAssertionError] if a MiniTest::Assertion error occurs
   def self.run(operation_type:, user:)
     test = nil
     ActiveRecord::Base.transaction do
@@ -53,19 +58,40 @@ class ProtocolTestEngine
   end
 end
 
+# Error class for non-syntax errors that occur during execution of protocol test.
 class KrillTestError < Krill::KrillBaseError
+
+  # Initialize a new error object.
+  #
+  # @param message [String] the error message
+  # @param error [Exception] the error object
+  # @param operation_type [OperationType] the protocol being tested
   def initialize(message: 'Error during test', error:, operation_type:)
     super(message: message, error: error, operation_type: operation_type)
   end
 end
 
+# Error class for test syntax errors.
 class KrillTestSyntaxError < Krill::KrillBaseError
+
+  # Initialize a new error object.
+  #
+  # @param message [String] the error message
+  # @param error [Exception] the error object
+  # @param operation_type [OperationType] the protocol being tested
   def initialize(message: 'Syntax error in test', error:, operation_type:)
     super(message: message, error: error, operation_type: operation_type)
   end
 end
 
+# Error class for an assertion failure in a test.
 class KrillAssertionError < Krill::KrillBaseError
+
+  # Initialize a new error object.
+  #
+  # @param message [String] the error message
+  # @param error [Exception] the error object
+  # @param operation_type [OperationType] the protocol being tested
   def initialize(message: 'Assertion failure', error:, operation_type:)
     super(message: message, error: error, operation_type: operation_type)
   end
