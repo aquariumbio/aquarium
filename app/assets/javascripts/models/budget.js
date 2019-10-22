92b01918ce6366c8a29c5381536a8c9fb851fed6
Ben Keller <bjkeller@uw.edu>
2019-10-22 08:59:35 -0400

    Format budget.js

--- app/assets/javascripts/models/budget.js
+++ app/assets/javascripts/models/budget.js
@@ -1,5 +1,4 @@
 AQ.Budget.record_getters.spent = function() {
-
   var budget = this;
   delete budget.spent;
   budget.spent = {};
@@ -7,19 +6,17 @@ AQ.Budget.record_getters.spent = function() {
   AQ.get("/budgets/" + budget.id + "/spent").then(response => {
     budget.spent = response.data;
     // AQ.update();
-  })
+  });
 
   return budget.spent;
-
-}
+};
 
 //
 // Asynchronously returns a list of budget ids corresponding to the year, month and
-// user_id given. 
+// user_id given.
 //
-AQ.Budget.used = function(year, month, user_id=-1) {
-
-  return AQ.get(`/invoices/budgets_used/${year}/${month}/${user_id}`)
-           .then(result => result.data)
-
-}
+AQ.Budget.used = function(year, month, user_id = -1) {
+  return AQ.get(`/invoices/budgets_used/${year}/${month}/${user_id}`).then(
+    result => result.data
+  );
+};
