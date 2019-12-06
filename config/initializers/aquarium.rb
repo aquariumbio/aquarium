750599328a02abf650d719da8a1ccccddb794322
Ben Keller <bjkeller@uw.edu>
2019-12-06 15:05:17 -0800

    Add environment variables to aq config

--- config/initializers/aquarium.rb
+++ config/initializers/aquarium.rb
@@ -3,31 +3,31 @@
 #
 # Installation specific stuff here
 #
+# TODO: allow loading from file to override these settings (maybe don't include environment variables)
 
 #
 # Name of the instance (Change to the name of your lab)
 #
-Bioturk::Application.config.instance_name = 'Your Lab'
+Bioturk::Application.config.instance_name = ENV['LAB_NAME'] || 'Your Lab'
 
 #
 # Landing page logo (Place file in aquarium/app/assets/images)
 #
-Bioturk::Application.config.logo_path = 'aquarium-logo.png'
+Bioturk::Application.config.logo_path = ENV['LOGO_PATH'] || 'aquarium-logo.png'
 
 #
 # URLs
 #
-Bioturk::Application.config.github_path = 'https://github.com/yourlab'
-Bioturk::Application.config.image_server_interface = 'http://localhost:9000/images/'
-Bioturk::Application.config.vision_server_interface = ''
+Bioturk::Application.config.image_server_interface = ENV['IMAGE_URI'] || 'http://localhost:9000/images/'
 
 #
 # Configuration
 #
 Bioturk::Application.config.debug_tools = true # false will hide debug buttons in planner and manager
-Bioturk::Application.config.krill_port = 3500
-Bioturk::Application.config.krill_hostname = 'krill' # for docker NAT
+Bioturk::Application.config.krill_port = ENV['KRILL_PORT'] || 3500
+Bioturk::Application.config.krill_hostname = ENV['KRILL_HOST'] || 'krill' # for docker NAT
 
+# TODO: move this elsewhere
 #
 # Set CSRF token names
 #
@@ -41,4 +41,4 @@ Bioturk::Application.config.angular_rails_csrf_options = {
 #       Also, make sure to set the environment variable AWS_REGION.
 #       See config/initializers/production.rb under "email".
 
-# Bioturk::Application.config.email_from_address = "aquarium@yourlab.org"
+Bioturk::Application.config.email_from_address = ENV['LAB_EMAIL_ADDRESS']
