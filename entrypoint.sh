a33541476822ddbfeac248a95392df2a3d094ed5
Ben Keller <bjkeller@uw.edu>
2019-12-06 14:10:03 -0800

    Update entrypoint
    Update entrypoint so that
    1. Servers call IP fix.
    2. IP fix done when no host name given or is localhost.
    3. Display license when production aquarium starts.

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,17 +23,26 @@ set -e
 unset BUNDLE_PATH
 unset BUNDLE_BIN
 
-# TODO: allow different name for s3 service
+# Fixes ip address for using a minio service from within the
+# local docker-compose configuration.  In this case, to talk to minio, paperclip
+# needs the minio host name to be localhost:9000, and we have to force that
+# address to point to the minio s3 service.
+#
+# This will apply fix if either S3_HOST is not set, or is set to localhost:9000.
 _fix_local_minio_ip() {
-    # see https://serverfault.com/questions/551487/dnat-from-localhost-127-0-0-1
-    echo "fix ip address for local s3"
-    S3_IP=`dig s3 +short`
-    iptables -t nat -A OUTPUT -m addrtype --src-type LOCAL --dst-type LOCAL -p tcp --dport 9000 -j DNAT --to-destination $S3_IP:9000
-    iptables -t nat -A POSTROUTING -m addrtype --src-type LOCAL --dst-type UNICAST -j MASQUERADE
+    echo "Fixing IP address for local minio"
+    if [ -z ${S3_HOST+x} ] || [ $(expr "$S3_HOST" : 'localhost.*') -gt 0 ]; then
+        # see https://serverfault.com/questions/551487/dnat-from-localhost-127-0-0-1
+        echo "fix ip address for local s3"
+        S3_IP=`dig s3 +short`
+        iptables -t nat -A OUTPUT -m addrtype --src-type LOCAL --dst-type LOCAL -p tcp --dport 9000 -j DNAT --to-destination $S3_IP:9000
+        iptables -t nat -A POSTROUTING -m addrtype --src-type LOCAL --dst-type UNICAST -j MASQUERADE
+    fi
 }
 
+# Wait for database to start.
 _wait_for_database() {
-    # wait for database to start
+    echo "waiting for database to respond"
     while ! nc -z ${DB_HOST:-'db'} ${DB_PORT:-3306}; do
         sleep 1 # wait for 1 second before check again
     done
@@ -47,20 +56,35 @@ _clean_up_stray_server() {
     fi
 }
 
+# Folds the license file into 80 columns, strips off the markdown header, and
+# sends the result to standard output.
+_show_license() {
+    fold -w 80 -s license.md | tail -n +3
+}
+
+# Starts the production server.
+# First fixes the local IP address for minio if needed, and shows the license.
 _start_production_server() {
+    _fix_local_minio_ip
     echo "Starting production Rails server"
+    _show_license
     exec puma -C config/production_puma.rb -e production
 }
 
+# Starts the production server.
+# First, fixes the local IP address for minio if needed.
 _start_development_server() {
+    _fix_local_minio_ip
     echo "Starting development Rails server"
     exec rails server -e development -p 3000 -b '0.0.0.0'
 }
 
+# Starts the krill server using the rails environment passed as an argument.
 _start_krill_server() {
     echo "Starting $1 Krill runner"
     exec rails runner -e $1 'Krill::Server.new.run(3500)'
 }
+
 
 _add_admin_user() {
     echo "adding admin user with environment variables"
@@ -75,31 +99,16 @@ _main() {
 
     _clean_up_stray_server
     _wait_for_database
-    
-    echo "here $1"
-    # fix minio ip address if either no host set or is localhost
-    match=`expr "$S3_HOST" : 'localhost.*'`
-    echo "host"
-    echo "match $match"
-    # if [ -z ${S3_HOST+x} ] || [ $match -gt 0 ]; then
-    #     echo "fixing minio"
-    #     _fix_local_minio_ip
-    # fi
 
     if [ $1 = "development" ]; then
-        echo "development"
         _start_development_server
-
     elif [ $1 = "production" ]; then
-        echo "production"
         _start_production_server
     elif [ $1 = "krill" ]; then
-        echo "krill"
         _start_krill_server $2
     else
         # If the normal image startup flags were not given as arguments, 
         # then exec whatever arguments were given
-        echo "other"
         exec "$@"
     fi
 }
