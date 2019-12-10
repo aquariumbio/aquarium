f013ea351fb6b74aac5094a8e99bf5daf158744e
Ben Keller <bjkeller@uw.edu>
2019-12-10 15:05:43 -0800

    Change aquarium config to allow use of config file

--- config/initializers/aquarium.rb
+++ config/initializers/aquarium.rb
@@ -3,29 +3,48 @@
 #
 # Installation specific stuff here
 #
-# TODO: allow loading from file to override these settings (maybe don't include environment variables)
+
+default_config = {
+  instance_name: 'Your Lab',
+  logo_path: 'aquarium-logo.png',
+  image_uri: 'http://localhost:9000/images/'
+}
+begin
+  instance_config = Bioturk::Application.config_for(:instance).symbolize_keys
+rescue StandardError => e
+  print("failed to read instance.yml #{e}")
+  instance_config = {}
+end
+instance_config = default_config.merge(instance_config)
+print(instance_config.to_s )
 
 #
 # Name of the instance (Change to the name of your lab)
 #
-Bioturk::Application.config.instance_name = ENV['LAB_NAME'] || 'Your Lab'
+Bioturk::Application.config.instance_name = ENV['INSTANCE_NAME'] || instance_config[:instance_name]
+
+#
+# Email address. Required if email service is enabled.
+#
+Bioturk::Application.config.email_from_address = ENV['LAB_EMAIL_ADDRESS'] || instance_config[:lab_email_address]
 
 #
 # Landing page logo (Place file in aquarium/app/assets/images)
 #
-Bioturk::Application.config.logo_path = ENV['LOGO_PATH'] || 'aquarium-logo.png'
+Bioturk::Application.config.logo_path = ENV['LOGO_PATH'] || instance_config[:logo_path]
 
 #
 # URLs
 #
-Bioturk::Application.config.image_server_interface = ENV['IMAGE_URI'] || 'http://localhost:9000/images/'
+Bioturk::Application.config.image_server_interface = ENV['IMAGE_URI'] || instance_config[:image_uri]
 
 #
 # Configuration
 #
-Bioturk::Application.config.debug_tools = true # false will hide debug buttons in planner and manager
 Bioturk::Application.config.krill_port = ENV['KRILL_PORT'] || 3500
 Bioturk::Application.config.krill_hostname = ENV['KRILL_HOST'] || 'krill' # for docker NAT
+
+Bioturk::Application.config.debug_tools = true # false will hide debug buttons in planner and manager
 
 # TODO: move this elsewhere
 #
@@ -36,9 +55,4 @@ Bioturk::Application.config.angular_rails_csrf_options = {
   header_name: "X-XSRF-TOKEN_#{Bioturk::Application.environment_name}"
 }
 
-# Emailer
-# Note: You must set up an AWS SimpleEmailService domain for this to work.
-#       Also, make sure to set the environment variable AWS_REGION.
-#       See config/initializers/production.rb under "email".
 
-Bioturk::Application.config.email_from_address = ENV['LAB_EMAIL_ADDRESS']
