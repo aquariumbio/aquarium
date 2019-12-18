60ef334ed3186b545e62358d34158c99b5112dcc
Ben Keller <bjkeller@uw.edu>
2019-12-18 10:29:32 -0800

    Fix rubocop issues and update todo

--- spec/models/operation_type_spec.rb
+++ spec/models/operation_type_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe OperationType, type: :model do
       category: 'testing',
       protocol: 'class Protocol; def main; show { title \'blah\' }; end; end',
       parameters: [
-        { name: 'string_param', type: 'string', choices: 'one,two'},
-        { name: 'number_param', type: 'number', choices: '1.1,2.2,3.3'}
+        { name: 'string_param', type: 'string', choices: 'one,two' },
+        { name: 'number_param', type: 'number', choices: '1.1,2.2,3.3' }
       ],
       user: test_user
     )
@@ -31,9 +31,9 @@ RSpec.describe OperationType, type: :model do
     it 'param protocol has fields for parameters' do
       types = param_protocol.field_types
       expect(types).not_to be_empty
-      string_type = types.find_by_name('string_param')
+      string_type = types.find_by(name: 'string_param')
       expect(string_type).not_to be_nil
-      number_type = types.find_by_name('number_param')
+      number_type = types.find_by(name: 'number_param')
       expect(number_type).not_to be_nil
     end
 
@@ -41,15 +41,15 @@ RSpec.describe OperationType, type: :model do
       no_param_protocol.field_types
       expect(no_param_protocol.field_types).to be_empty
       no_param_protocol.update_field_types([
-        { name: 'string_param', ftype: 'string', choices: 'one,two'},
-        { name: 'number_param', ftype: 'number', choices: '1.1,2.2,3.3'}
-      ])
+                                             { name: 'string_param', ftype: 'string', choices: 'one,two' },
+                                             { name: 'number_param', ftype: 'number', choices: '1.1,2.2,3.3' }
+                                           ])
       types = no_param_protocol.field_types
       expect(types).not_to be_empty
-      string_type = types.find_by_name('string_param')
+      string_type = types.find_by(name: 'string_param')
       expect(string_type).not_to be_nil
       expect(string_type.choices).to eq('one,two')
-      number_type = types.find_by_name('number_param')
+      number_type = types.find_by(name: 'number_param')
       expect(number_type).not_to be_nil
       expect(number_type.choices).to eq('1.1,2.2,3.3')
     end
