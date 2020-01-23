2afcc5c744f5ac2daff9e82065211a8b6516107f
Ben Keller <bjkeller@uw.edu>
2020-01-23 11:28:13 -0800

    Change setup to assign s3 key

--- setup.sh
+++ setup.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-new_key=`openssl rand -hex 64`
+new_key_base=`openssl rand -hex 64`
+s3_key=`openssl rand -hex 40`
 ENV_FILE=.env
 
 if [[ -f "$ENV_FILE" ]]; then
     if grep -Fq "SECRET_KEY_BASE=" $ENV_FILE; then
-        sed -i '' "s/^SECRET_KEY_BASE=.*/SECRET_KEY_BASE=$new_key/g"  $ENV_FILE
+        sed -i '' "s/^SECRET_KEY_BASE=.*/SECRET_KEY_BASE=$new_key_base/g"  $ENV_FILE
     else
-        echo 'SECRET_KEY_BASE='$new_key >> $ENV_FILE
+        echo 'SECRET_KEY_BASE='$new_key_base >> $ENV_FILE
     fi
 else
     echo 'DB_NAME=production' >> $ENV_FILE 
     echo 'DB_USER=aquarium' >> $ENV_FILE
     echo 'DB_PASSWORD=aSecretAquarium' >> $ENV_FILE
+    echo 'S3_SERVICE=minio' >> $ENV_FILE
     echo 'S3_ID=aquarium_minio' >> $ENV_FILE
-    echo 'S3_SECRET_ACCESS_KEY=KUNAzqrNifmM6GwNVZ8IP7dxZAkYjhnwc0bfdz0W' >> $ENV_FILE
+    echo 'S3_SECRET_ACCESS_KEY='$s3_key >> $ENV_FILE
     echo 'TIMEZONE=America/Los_Angeles' >> $ENV_FILE
-    echo 'SECRET_KEY_BASE='$new_key >> $ENV_FILE 
+    echo 'SECRET_KEY_BASE='$new_key_base >> $ENV_FILE 
 fi
 
 
