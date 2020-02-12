abb7598654698b5b2489e566f064f705c670cf8a
Ben Keller <bjkeller@uw.edu>
2020-02-12 15:07:26 -0800

    Script getting timezone

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 new_key_base=`openssl rand -hex 64`
 s3_key=`openssl rand -hex 40`
+timezone=`curl https://ipapi.co/timezone`
 ENV_FILE=.env
 
 if [[ -f "$ENV_FILE" ]]; then
@@ -16,7 +17,7 @@ else
     echo 'S3_SERVICE=minio' >> $ENV_FILE
     echo 'S3_ID=aquarium_minio' >> $ENV_FILE
     echo 'S3_SECRET_ACCESS_KEY='$s3_key >> $ENV_FILE
-    echo 'TIMEZONE=America/Los_Angeles' >> $ENV_FILE
+    echo 'TIMEZONE='$timezone >> $ENV_FILE
     echo 'SECRET_KEY_BASE='$new_key_base >> $ENV_FILE 
 fi
 
