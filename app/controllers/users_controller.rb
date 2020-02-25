4fa0210087ece451f11d013ca63d1422f9373bcc
lekhacminhphuong <leanutella192@gmail.com>
2020-02-25 10:45:28 -0800

    Reformat Error Page for Create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
         flash[:success] = "#{params[:user][:name]} has been assimilated."
         redirect_to @user
       else
-        render 'new'
+        render layout: 'aq2', action: 'new' 
       end
 
     else
