c35c39ab1f9b9159e9128763c40dd5986b185678
Ben Keller <bjkeller@uw.edu>
2020-03-09 14:09:21 -0700

    Create initial database from default on setup

--- setup.sh
+++ setup.sh
@@ -22,6 +22,11 @@ else
     echo 'SECRET_KEY_BASE='$new_key_base >> $ENV_FILE 
 fi
 
+DB_INIT_DIR=./data/mysql_init
+DB_FILE=$DB_INIT_DIR/dump.sql
+if [[ ! -f "$DB_FILE" ]]; then
+    cp $DB_INIT_DIR/default.sql $DB_INIT_DIR/dump.sql
+fi
 
 # TODO: allow user to set other values
 # TODO: make this a git post-checkout hook, though don't replace secret_key_base
