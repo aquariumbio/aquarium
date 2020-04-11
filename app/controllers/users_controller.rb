0913a08541b4ca5e4625eb868eb898401989ad2e
Ben Keller <bjkeller@uw.edu>
2020-04-11 11:20:49 -0700

    Return current user memberships as json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -150,7 +150,7 @@ class UsersController < ApplicationController
 
   def current
     u = current_user.as_json
-    u[:memberships] = current_user.groups
+    u[:memberships] = current_user.groups.as_json
     render json: u
   end
 
