66b1946993560461de6b77857d9969b2d8c38e85
Ben Keller <bjkeller@uw.edu>
2020-05-03 10:03:19 -0700

    Add spec for budget controller

---
+++ spec/controllers/budgets_controller_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BudgetsController, type: :controller do
+  let!(:regular_user) { create(:user) }
+
+  
+  it 'non-admin should return error message' do
+    token_name = "remember_token_#{Bioturk::Application.environment_name}".to_sym
+    cookies[token_name] = User.find(regular_user.id).remember_token
+
+    get :index, as: :json 
+    expect(response.headers['Content-Type']).to eq('text/html; charset=utf-8')
+    expect(response.body).to be_nil
+  end
+
+
+
+end
