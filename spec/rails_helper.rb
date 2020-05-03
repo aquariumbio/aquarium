ab1022ff3b69c663030a513ace3c7bfdf116a04d
Ben Keller <bjkeller@uw.edu>
2020-05-02 21:01:32 -0700

    Add groups

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_group 'Models', ['app/models', 'app/helpers']
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Krill', ['app/krill', 'lib/krill']
+  add_group 'Planner', 'app/planner'
+  add_group 'Planning', 'app/planning'
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'development'
