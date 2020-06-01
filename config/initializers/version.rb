cba8ed2894992d842c59420c2ce14de2f1058f43
Ben Keller <bjkeller@uw.edu>
2020-06-01 15:49:58 -0700

    Update version

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,6 +1,6 @@
 # typed: false
 # frozen_string_literal: true
 
-Bioturk::Application.config.aquarium_version = '2.7.3'
+Bioturk::Application.config.aquarium_version = '2.8.0'
 
 puts "Aquarium Version #{Bioturk::Application.config.aquarium_version} Starting!"
