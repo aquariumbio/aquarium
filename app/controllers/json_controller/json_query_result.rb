e003efadd4a66ed21e4fff7b4771069810aaea8e
Ben Keller <bjkeller@uw.edu>
2020-08-25 09:25:18 -0700

    Make associations methods use recent filter
    Make associations methods use recent filter.
    1. Move filter method from json query result class to data association class.
    2. Change data associator associations method use filtering method.
    3. Change names of scope and filter methods.

--- app/controllers/json_controller/json_query_result.rb
+++ app/controllers/json_controller/json_query_result.rb
@@ -13,7 +13,7 @@ class JsonController
 
       model = get_model(model: parameters[:model], include: parameters[:include])
       result = apply_method(model: model, method: parameters[:method], arguments: parameters[:arguments], id: parameters[:id], options: parameters[:options])
-      result = filter_associations(result) if parameters[:model] == 'DataAssociation'
+      result = DataAssociation.select_most_recent(result) if parameters[:model] == 'DataAssociation'
 
       return result.as_json(include: parameters[:include], methods: parameters[:methods]) if parameters[:methods] && parameters[:include]
       return result.as_json(methods: parameters[:methods]) if parameters[:methods] && !parameters[:include]
@@ -111,18 +111,6 @@ class JsonController
       true
     end
 
-    # Filters data associations by key.
-    # Selects the most recently updated record, and in case of ties selects by largest id.
-    #
-    # @param associations [ActiveRecord<DataAssociation>] the data associations
-    # @return [Array<DataAssociation]
-    def self.filter_associations(associations)
-      associations
-        .order(:key, :updated_at, :id)
-        .group_by(&:key)
-        .map { |_, group| group.last }
-    end
-
-    private_class_method :apply_method, :gather_includes, :association?, :method_ok?, :filter_associations
+    private_class_method :apply_method, :gather_includes, :association?, :method_ok?
   end
 end
