77540bb88acb1cedd54e51a0823d8e77494455c3
Ben Keller <bjkeller@uw.edu>
2020-09-04 12:32:04 -0700

    Update version to 2.8.1

--- setup.sh
+++ setup.sh
@@ -49,7 +49,7 @@ if [[ ! -f "$ENV_FILE" ]]; then
     touch $ENV_FILE
 fi
 
-_set_variable 'AQUARIUM_VERSION' '2.8.0'
+_set_variable 'AQUARIUM_VERSION' '2.8.1'
 _set_variable 'APP_PUBLIC_PORT' '80'
 _set_variable 'S3_PUBLIC_PORT' '9000'
 _set_variable 'DB_NAME' 'production'
