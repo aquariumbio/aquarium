0cc78dc00627f5346042229b4eefbba1cb99304a
gnomicosuw <gene@Genes-MacBook-Pro.local>
2020-10-05 09:29:56 -0700

    job count

--- app/controllers/api/v2/groups_controller.rb
+++ app/controllers/api/v2/groups_controller.rb
@@ -29,22 +29,53 @@ class Api::V2::GroupsController < ApplicationController
     group = Group.find(id) rescue nil
     render json: api_error( { "group_id" => ["invalid group"] } ) and return if !group
 
-    # TODO: THIS IS A BAND-AID UNTIL WE MOVE TO V2
-    sql="
-      select u.id, u.name, u.login
-      from groups g
-      inner join memberships m on m.group_id = g.id
-      inner join users u on u.id = m.user_id
-      where g.id = #{id}
-      order by u.name, u.login
-    "
-    res = User.find_by_sql sql
+    job_count = false
+    params[:options].to_a.each do |s|
+      job_count = true if s == "job_count"
+    end
 
-    # HACK TO PREVENT AS_JSON OVERRIDE (DEF AS_JSON IN USER.RB)
-    # TODO: REMOVE ONCE DEF AS_JSON IS FIXED
-    result = []
-    res.each do |r|
-      result << { :id => r.id, :name => r.name, :login => r.login }
+    # TODO: MOVE SQL TO MODEL
+    # TODO: THIS IS A BAND-AID UNTIL WE MOVE TO VE
+    if job_count
+      sql="
+        select u.id, u.name, u.login, jc.n
+        from groups g
+        inner join memberships m on m.group_id = g.id
+        inner join users u on u.id = m.user_id
+        left join (
+          select assigned_to, count(*) as 'n'
+          from view_job_assignments
+          where pc != -2
+          group by assigned_to
+        ) jc on jc.assigned_to = u.id
+        where g.id = #{id}
+        order by u.name, u.login
+      "
+      res = User.find_by_sql sql
+
+      # HACK TO PREVENT AS_JSON OVERRIDE (DEF AS_JSON IN USER.RB)
+      # TODO: REMOVE ONCE DEF AS_JSON IS FIXED
+      result = []
+      res.each do |r|
+        result << { :id => r.id, :name => r.name, :login => r.login, :job_count => r.n }
+      end
+    else
+      sql="
+        select u.id, u.name, u.login
+        from groups g
+        inner join memberships m on m.group_id = g.id
+        inner join users u on u.id = m.user_id
+        where g.id = #{id}
+        order by u.name, u.login
+      "
+      res = User.find_by_sql sql
+
+      # HACK TO PREVENT AS_JSON OVERRIDE (DEF AS_JSON IN USER.RB)
+      # TODO: REMOVE ONCE DEF AS_JSON IS FIXED
+      result = []
+      res.each do |r|
+        result << { :id => r.id, :name => r.name, :login => r.login }
+      end
     end
 
     render json: api_ok(result)
