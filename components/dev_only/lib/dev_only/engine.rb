6145e6f35f11179326a2b8dd2bc7be40fb337947
Ben Keller <bjkeller@uw.edu>
2020-10-14 13:21:10 -0700

    Add precompile for dev_only assets

--- components/dev_only/lib/dev_only/engine.rb
+++ components/dev_only/lib/dev_only/engine.rb
@@ -1,5 +1,9 @@
 module DevOnly
   class Engine < ::Rails::Engine
     isolate_namespace DevOnly
+
+    initializer "dev_only.assets.precompile" do |app|
+      app.config.assets.precompile += %w( dev_only/application.js dev_only/application.css )
+    end
   end
 end
