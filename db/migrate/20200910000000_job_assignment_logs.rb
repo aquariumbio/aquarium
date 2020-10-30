46dc784a2a69d4dffe8e0374dbd53b77def30ddf
Ben Keller <bjkeller@uw.edu>
2020-10-30 07:31:59 -0700

    Pull updated job assignment migration

--- db/migrate/20200910000000_job_assignment_logs.rb
+++ db/migrate/20200910000000_job_assignment_logs.rb
@@ -14,22 +14,25 @@ class JobAssignmentLogs < ActiveRecord::Migration
       t.index :assigned_to
     end
 
-    add_foreign_key :job_assignment_logs, :jobs, on_delete: :cascade
-    add_foreign_key :job_assignment_logs, :users, column: "assigned_by", on_delete: :cascade
-    add_foreign_key :job_assignment_logs, :users, column: "assigned_to", on_delete: :cascade
+    add_foreign_key :job_assignment_logs, :jobs, on_delete: :cascade                         rescue nil
+    add_foreign_key :job_assignment_logs, :users, column: "assigned_by", on_delete: :cascade rescue nil
+    add_foreign_key :job_assignment_logs, :users, column: "assigned_to", on_delete: :cascade rescue nil
     change_column_null :job_assignment_logs, :created_at, false
     change_column_null :job_assignment_logs, :updated_at, false
 
     # VIEW - CURRENT JOB ASSIGNMENT
     execute <<-SQL
+      create view view_job_assignment_logs as
+      select max(id) as 'id'
+      from job_assignment_logs
+      group by job_id
+    SQL
+
+    execute <<-SQL
       create view view_job_assignments as
       select jal.*, j.pc, ub.name as 'by_name', ub.login as 'by_login', ut.name as 'to_name', ut.login as 'to_login'
       from job_assignment_logs jal
-      inner join (
-        select max(id) as 'id'
-        from job_assignment_logs
-        group by job_id
-      ) ij on ij.id = jal.id
+      inner join view_job_assignment_logs vjal on vjal.id = jal.id
       inner join jobs j on j.id = jal.job_id
       inner join users ub on ub.id = jal.assigned_by
       inner join users ut on ut.id = jal.assigned_to
