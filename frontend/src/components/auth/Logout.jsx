5003f8908c32a432eab3d500ed432a8f4143d176
Mariko Anderson <marikoa@uw.edu>
2020-11-04 10:54:38 -0800

    Add eslint with Airbnb styling guide, clean up styling errors

--- frontend/src/components/auth/Logout.jsx
+++ frontend/src/components/auth/Logout.jsx
@@ -1,47 +1,49 @@
-import React, { useState, useEffect, } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
-import { useHistory, Redirect, } from "react-router-dom";
-import axios from "axios";
+import { useHistory, Redirect } from 'react-router-dom';
+import axios from 'axios';
 
-export default function Logout(props) {
-  let history = useHistory();
+export default function Logout() {
+  const history = useHistory();
 
-  const [ logOutErrors, setLoginOutError ] = useState();
-  const [ logout, setlogout ] = useState(false);
+  const [logOutErrors, setLoginOutError] = useState();
+  const [logout, setlogout] = useState(false);
 
   useEffect(() => {
-    if (logout && !logOutErrors !== "") {
+    if (logout && !logOutErrors !== '') {
       // storetoken in session storage to keep user logged in between page refreshes
       sessionStorage.clear('token');
     }
   });
 
-  const handleSignOut = (event) => {
+  const handleSignOut = () => {
     const token = sessionStorage.getItem('token');
 
     axios
-    .post(`user/sign_out?token=${token}`)
-    .then(response => {
-      if (response.data.status === 200) {
-        setlogout(true);
-        history.push("/login");
-        window.location.reload();
-      }
+      .post(`user/sign_out?token=${token}`)
+      .then((response) => {
+        if (response.data.status === 200) {
+          setlogout(true);
+          history.push('/login');
+          window.location.reload();
+        }
 
-      if (response.data.status !== 200) {
-        return setLoginOutError(response.data.error)
-      }
-    })
-  }
-  if (!sessionStorage.getItem("token")) {
+        if (response.data.status !== 200) {
+          return setLoginOutError(response.data.error);
+        }
+      });
+  };
+  if (!sessionStorage.getItem('token')) {
     return <Redirect to="/login" />;
   }
   return (
-    <Button name="sign_out"
-            primary
-            type="button"
-            onClick={handleSignOut}>
+    <Button
+      name="sign_out"
+      primary
+      type="button"
+      onClick={handleSignOut}
+    >
       SIGN OUT
     </Button>
-  )
+  );
 }
