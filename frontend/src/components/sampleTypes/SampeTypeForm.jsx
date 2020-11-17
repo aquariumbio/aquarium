92afb6057399a62e58aca65eacca57a828291b39
Mariko Anderson <marikoa@uw.edu>
2020-11-17 10:23:45 -0600

    Add container and styling

--- frontend/src/components/sampleTypes/SampeTypeForm.jsx
+++ frontend/src/components/sampleTypes/SampeTypeForm.jsx
@@ -1,8 +1,31 @@
-import React, { useState } from 'react';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import React, { useState } from 'react';
+
+const useStyles = makeStyles(() => ({
+  container: {
+    minWidth: 'lg',
+  },
+  title: {
+    fontSize: '2.5rem',
+    fontWeight: '700',
+    marginTop: '12px',
+    marginBottom: '30px',
+  },
+  name: {
+    fontSize: '1rem',
+    fontWeight: '700',
+    margin: '10px 0',
+  },
+  input: {
+
+  },
+}));
 
 const SampleTypeForm = () => {
+  const classes = useStyles();
   const [sampleTypeName, setSampleTypeName] = useState('');
 
   const handleSubmit = (event) => {
@@ -13,15 +36,18 @@ const SampleTypeForm = () => {
   };
 
   return (
-    <>
-      <Typography variant="h1" align="center">
+    <Container maxWidth="xl" minWidth>
+      <Typography variant="h1" align="center" className={classes.title}>
         Defining New Sample Type
       </Typography>
 
-      <Typography variant="h4">Name</Typography>
+      <Typography variant="h4" className={classes.name}>
+        Name
+      </Typography>
       <form name="sampe_type_definition_form" onSubmit={handleSubmit}>
         <TextField
           name="sample_type_name"
+          fullWidth
           value={sampleTypeName}
           id="sample_type_name"
           label="sample type name"
@@ -29,10 +55,11 @@ const SampleTypeForm = () => {
           onChange={(event) => setSampleTypeName(event.target.value)}
           variant="outlined"
           required
+          // TODO: Error HANDLING -- ONLY SHOW HELPER TEXT ON ERROR
           helperText="Sample name is required."
         />
       </form>
-    </>
+    </Container>
   );
 };
 
