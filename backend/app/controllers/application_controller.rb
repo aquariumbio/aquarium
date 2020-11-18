8214528b30ff0711b6619c5c8e41bf9aa52eefa7
Ben Keller <bjkeller@uw.edu>
2020-11-18 11:41:39 -0800

    Refactor token validation

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -5,23 +5,9 @@ class ApplicationController < ActionController::API
   # CHECK THE TOKEN AGAINST SPECIFIC PERMISSION_ID (0 = ANYTHING NOT RETIRED)
   # RETURN STATUS <AND> ( ERROR <OR> USER )
   def check_token_for_permission(permission_id = 0)
-    ip = request.remote_ip
+    ip = request.remote_ip.to_s
     token = params[:token].to_s.strip.downcase
 
-    status_code, datum = User.validate_token({ ip: ip, token: token }, permission_id)
-    case status_code
-    when 401
-      status = "unauthorized"
-      response = { error: datum }
-    when 403
-      status = "forbidden"
-      permission = Permission.permission_ids[permission_id]
-      error = permission ? "#{permission.capitalize} permissions required." : 'Forbidden.'
-      response = { error: error }
-    when 200
-      status = "ok"
-      response = { user: datum }
-    end
-    return status, response
+    User.validate_token(ip: ip, token: token, check_permission_id: permission_id)
   end
 end
