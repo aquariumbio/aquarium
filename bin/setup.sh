e2da4ff845c8c74db82b2823348ca2a154299fc3
Ben Keller <bjkeller@uw.edu>
2020-12-02 08:09:18 -0800

    Update setup script
    Update setup script
    1. Make function to write files for an environment.
    2. Change to write service-specific db variables to service file.
    3. Add write of public port variables.

--- bin/setup.sh
+++ bin/setup.sh
@@ -52,31 +52,45 @@ _set_timezone() {
     fi
 }
 
-mkdir -p $ENV_DIR/development
-sub_dir=$ENV_DIR/development
-env_file=$sub_dir/backend
-touch $env_file
-_set_variable 'DB_HOST' 'db' $env_file
-_set_variable 'DB_PORT' '3306' $env_file
-_set_variable 'SESSION_TIMEOUT' '15' $env_file
+_create_config() {
+    local environment=$1
+    local sub_dir=$ENV_DIR/$environment
 
+    mkdir -p $sub_dir
 
-env_file=$ENV_DIR/development/db
-touch $env_file
-db_name='aquarium_development'
-_set_variable 'DB_NAME' $db_name $env_file
-_set_variable 'MYSQL_DATABASE' $db_name $env_file
-db_user='aquarium'
-_set_variable 'DB_USER' $db_user $env_file
-_set_variable 'MYSQL_USER' $db_user $env_file
-db_password='aSecretAquarium'
-_set_variable 'DB_PASSWORD' $db_password $env_file
-_set_variable 'MYSQL_PASSWORD' $db_password $env_file
-_set_variable 'MYSQL_ROOT_PASSWORD' $db_password $env_file
+    local db_name="aquarium_$environment"
+    local db_user=$2
+    local db_password=$3
 
-env_file=$sub_dir/timezone
-touch $env_file
-_set_timezone $env_file
+    local env_file=$sub_dir/backend
+    touch $env_file
+    _set_variable 'BACKEND_PUBLIC_PORT' '3001' $env_file
+    _set_variable 'DB_HOST' 'db' $env_file
+    _set_variable 'DB_PORT' '3306' $env_file
+    _set_variable 'DB_NAME' $db_name $env_file
+    _set_variable 'DB_PASSWORD' $db_password $env_file
+    _set_variable 'DB_USER' $db_user $env_file
+    _set_variable 'SESSION_TIMEOUT' '15' $env_file
+
+    env_file=$sub_dir/frontend
+    touch $env_file
+    _set_variable 'FRONTEND_PUBLIC_PORT' '3001' $env_file
+
+    env_file=$sub_dir/db
+    touch $env_file
+    _set_variable 'MYSQL_DATABASE' $db_name $env_file
+    _set_variable 'MYSQL_USER' $db_user $env_file
+    _set_variable 'MYSQL_PASSWORD' $db_password $env_file
+    _set_variable 'MYSQL_ROOT_PASSWORD' $db_password $env_file
+    _set_variable 'DB_PUBLIC_PORT' '3307' $env_file
+
+    env_file=$sub_dir/timezone
+    touch $env_file
+    _set_timezone $env_file
+}
+
+_create_config 'development' 'aquarium' 'aSecretAquarium'
+_create_config 'production' 'aquarium' 'aSecretAquarium'
 
 # TODO: allow user to set other values
 # TODO: make this a git post-checkout hook, though don't replace secret_key_base
