96e659157a19fb2016f7f7ba4efbd82277a11be0
Ben Keller <bjkeller@uw.edu>
2020-12-03 11:05:42 -0800

    Tweak sql for database privs

--- docker/mysql_init/create_aquarium_test.sql
+++ docker/mysql_init/create_aquarium_test.sql
@@ -13,7 +13,7 @@
 -- CREATE DATABASE "aquarium_test" ----------------------------------
 DROP DATABASE IF EXISTS `aquarium_test`;
 CREATE DATABASE `aquarium_test` CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-GRANT ALL PRIVILEGES ON aquarium_test.* TO 'aquarium'@'%';
+GRANT ALL ON `aquarium_test`.* TO 'aquarium'@'%' ;
 USE `aquarium_test`;
 -- ---------------------------------------------------------
 
