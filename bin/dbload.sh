5b3981672c62b5a3f9669d60cb12823348ab4f33
Ben Keller <bjkeller@uw.edu>
2020-12-03 13:13:05 -0800

    Add script to load a database dump

---
+++ bin/dbload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -uxo pipefail
+
+if [[ $# -ne 2 ]]; then
+    echo "Expected 2 arguments: environment database-name"
+    exit 2
+fi
+
+ENV_DIR=.env_files/$1
+USER=`perl -lne '/^DB_USER=(.*)/ && print "$1"' $ENV_DIR/backend`
+PASSWORD=`perl -lne '/^DB_PASSWORD=(.*)/ && print "$1"' $ENV_DIR/backend`
+DATABASE=`perl -lne '/^DB_NAME=(.*)/ && print "$1"' $ENV_DIR/backend`
+
+cat $2 | docker-compose exec -T db mysql -u "$USER" -p"$PASSWORD" "$DATABASE"
