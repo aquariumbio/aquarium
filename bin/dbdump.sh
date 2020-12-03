b546568f36f3ac1e2dbbb7ea752570f0c631c51c
Ben Keller <bjkeller@uw.edu>
2020-12-03 13:12:47 -0800

    Fix reference to env_files

--- bin/dbdump.sh
+++ bin/dbdump.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -uxo pipefail
 
-ENV_DIR=.env/$1
+if [[ $# -ne 1 ]]; then
+    echo "Expected 1 argument: environment"
+    exit 2
+fi
+
+ENV_DIR=.env_files/$1
 USER=`perl -lne '/^DB_USER=(.*)/ && print "$1"' $ENV_DIR/backend`
 PASSWORD=`perl -lne '/^DB_PASSWORD=(.*)/ && print "$1"' $ENV_DIR/backend`
 DATABASE=`perl -lne '/^DB_NAME=(.*)/ && print "$1"' $ENV_DIR/backend`
