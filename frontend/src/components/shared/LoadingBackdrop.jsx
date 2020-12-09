83b9780ee2cb43ae0262f73bd5b9f78348db378c
Mariko Anderson <marikoa@uw.edu>
2020-12-09 11:09:36 -0800

    Create shared loading backdrop component

---
+++ frontend/src/components/shared/LoadingBackdrop.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import PropTypes from 'prop-types';
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+
+}));
+
+const LoadingBackdrop = React.forwardRef(({ isLoading }, ref) => {
+  const classes = useStyles();
+
+  return (
+    <Backdrop className={classes.backdrop} open={isLoading} ref={ref}>
+      <CircularProgress color="inherit" />
+    </Backdrop>
+  );
+});
+
+export default LoadingBackdrop;
+
+LoadingBackdrop.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+};
