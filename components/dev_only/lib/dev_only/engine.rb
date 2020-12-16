ba235f4f7317efbcd1f9a3d1a9e5b4049198fd60
Ben Keller <bjkeller@uw.edu>
2020-12-16 09:20:17 -0800

    Apply formatting

--- components/dev_only/lib/dev_only/engine.rb
+++ components/dev_only/lib/dev_only/engine.rb
@@ -3,7 +3,7 @@ module DevOnly
     isolate_namespace DevOnly
 
     initializer "dev_only.assets.precompile" do |app|
-      app.config.assets.precompile += %w( dev_only/application.js dev_only/application.css )
+      app.config.assets.precompile += %w(dev_only/application.js dev_only/application.css)
     end
   end
 end
