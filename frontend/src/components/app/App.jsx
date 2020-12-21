31a501ab89a3f84f337db6483e1c10c265af45b3
Mariko Anderson <marikoa@uw.edu>
2020-12-21 07:19:06 -0800

    Add location to redirect

--- frontend/src/components/app/App.jsx
+++ frontend/src/components/app/App.jsx
@@ -3,8 +3,7 @@
 import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 // eslint-disable-next-line object-curly-newline
-import { Redirect, Route, Switch } from 'react-router-dom';
-import API from '../../helpers/API';
+import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
 import AnnouncementsPage from '../announcements/AnnouncementsPage';
 import LoginDialog from '../auth/LoginDialog';
 import BudgetsPage from '../budgets/BudgetsPage';
@@ -30,6 +29,7 @@ import SampleTypeDefinitionForm from '../sampleTypes/SampeTypeDefinitionForm';
 import SampleTypesPage from '../sampleTypes/SampleTypesPage';
 import UserProfilePage from '../users/UserProfilePage';
 import UsersPage from '../users/UsersPage';
+import API from '../../helpers/API';
 
 const useStyles = makeStyles({});
 const theme = createMuiTheme({
@@ -42,12 +42,21 @@ const theme = createMuiTheme({
 
 export default function App() {
   const classes = useStyles();
+  const location = useLocation();
+
   return (
     <ThemeProvider theme={theme}>
       <div name="app-container" className={classes.container} data-cy="app-container">
         { /* Users cannot interact with the app if they do not have a token */
-          (!sessionStorage.getItem('token') || !API.tokens.isAuthenticated)
-          && <Redirect to="/login" />
+          (!API.tokens.isAuthenticated || !sessionStorage.getItem('token'))
+          && (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location },
+            }}
+          />
+          )
         }
         <Switch>
           <Route path="/login" render={(props) => <LoginDialog {...props} />} />
@@ -59,8 +68,11 @@ export default function App() {
 
             {/* Left Hamburger Menu */}
             <Route exact path="/users" render={(props) => <UsersPage {...props} />} />
+
             <Route exact path="/sample_types" render={(props) => <SampleTypesPage {...props} />} />
             <Route exact path="/sample_types/new" render={(props) => <SampleTypeDefinitionForm {...props} />} />
+            <Route exact path="/sample_types/:id/edit" render={(props) => <SampleTypeDefinitionForm {...props} />} />
+
             <Route exact path="/announcements" render={(props) => <AnnouncementsPage {...props} />} />
             <Route exact path="/budgets" render={(props) => <BudgetsPage {...props} />} />
             <Route exact path="/object_types" render={(props) => <ContainersPage {...props} />} />
