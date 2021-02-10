51dad718e8b75b9cddd60103bf5f9884eb984172
Ben Keller <bjkeller@uw.edu>
2021-02-10 13:21:50 -0800

    Add spec for accessing upload contents using url

--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -1,6 +1,8 @@
 # typed: false
 # frozen_string_literal: true
 
+require 'csv'
+require 'open-uri'
 require 'rails_helper'
 
 RSpec.describe Upload, type: :model do
@@ -9,7 +11,7 @@ RSpec.describe Upload, type: :model do
 
   context 'basics' do
 
-    def new_upload
+    def logo_upload
 
       u = Upload.new
 
@@ -23,8 +25,20 @@ RSpec.describe Upload, type: :model do
 
     end
 
+    def csv_upload
+      u = Upload.new
+
+      file = StringIO.new("column1,column2\n0.5,0.3\n")
+      u.upload = file
+      u.name = 'dummyfile'
+      u.save
+
+      u
+    end
+
     it 'can be created' do
-      expect { new_upload }.not_to raise_error
+      expect { logo_upload }.not_to raise_error
+      expect { csv_upload }.not_to raise_error
     end
 
     let!(:test_user) { create(:user) }
@@ -47,7 +61,21 @@ RSpec.describe Upload, type: :model do
     end
     it 'can be associated with a model via a data association' do
       operation = io_protocol.create_operation(status: 'waiting', user_id: test_user.id)
-      operation.associate :my_file, 'This is a test file', new_upload
+      operation.associate :my_file, 'This is a test file', logo_upload
+    end
+
+    it 'expect upload to be usable' do
+      upload = csv_upload
+
+      expect(upload.url).not_to be_nil
+
+      file = nil
+      open(upload.url) do |io|
+        expect { file = CSV.read(io) }.not_to raise_error
+      end
+
+      expect(file).not_to be_nil
+      expect(file[0][0]).to eq('column1')
     end
 
   end
