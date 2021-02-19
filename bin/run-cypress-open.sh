c286103487a0b5a4fabcaedad117eabfeab31d5a
Ben Keller <bjkeller@uw.edu>
2021-02-19 15:55:27 -0800

    Remove files related to cypress open

--- bin/run-cypress-open.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -uxo pipefail
-
-docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.cypress.yml -f docker-compose.cypress-open.yml up --exit-code-from e2e
