f7a31c2431a0e10cc3b115bb2dc71f724fc79996
Ben Keller <bjkeller@uw.edu>
2021-02-25 15:34:20 -0800

    Allow connection via backend service name

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -49,5 +49,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "aquarium"
+  config.hosts << "backend"
 end
