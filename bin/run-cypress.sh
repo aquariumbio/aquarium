a7a2bfb030b8e740075cc42fb6d40a1b4e24b8fc
Ben Keller <bjkeller@uw.edu>
2021-02-25 15:35:06 -0800

    Use test config for cypress runs

--- bin/run-cypress.sh
+++ bin/run-cypress.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -uxo pipefail
 
-docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.cypress.yml up --exit-code-from e2e
+docker-compose -f docker-compose.yml -f docker-compose.test.yml -f docker-compose.cypress.yml up --exit-code-from e2e
