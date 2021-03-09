3441eb95bbb1ca916a29bbc1e05a5da23dfaa7bf
Mariko Anderson <marikoa@uw.edu>
2021-03-09 12:12:27 -0800

    Remove duplicate files

--- frontend/src/components/shared/DropdownMenu.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import DropdownMenu from './DropdownMenu';
-
-describe('DropdownMenu', () => {
-  it.todo('should be closed at first render');
-  it.todo('should open the menu when the button is clicked');
-  it.todo('should close when an item is clicked');
-});
