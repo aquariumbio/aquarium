4def106fc5621e4d0d9a98372767b1caac7677e5
Ben Keller <bjkeller@uw.edu>
2021-03-09 07:11:47 -0800

    Hard code backend test service

--- end2end/cypress/support/commands.js
+++ end2end/cypress/support/commands.js
@@ -15,9 +15,10 @@ Cypress.Commands.add('login', () => {
   const login = 'neptune';
   const password = 'aquarium';
 
+  // want to be able to use ${Cypress.env(BASE_API)} for the URL, but not working here
   cy.request({
     method: 'POST',
-    url: `http://${Cypress.env(BASE_API)}/api/v3/token/create?login=${login}&password=${password}`,
+    url: `http://backend_test:3000/api/v3/token/create?login=${login}&password=${password}`,
   })
     .then((resp) => {
       window.localStorage.setItem('token', resp.body.token);
@@ -29,7 +30,7 @@ Cypress.Commands.add('logout', () => {
 
   cy.request({
     method: 'POST',
-    url: `http://${Cypress.env(BASE_API)}/api/v3/token/delete?token=${token}`,
+    url: `http://backend_test:3000/api/v3/token/delete?token=${token}`,
   })
     // eslint-disable-next-line no-unused-vars
     .then((resp) => {
