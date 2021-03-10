51ba7d9d944b351d5b1ed429b82c663dcd23d1eb
Ben Keller <bjkeller@uw.edu>
2021-03-10 14:42:29 -0800

    Move db env files for test to own file

--- bin/aquarium-test.sh
+++ bin/aquarium-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -uxo pipefail
 
-docker-compose -f docker-compose.yml -f docker-compose.test.yml $@
+docker-compose -f docker-compose.yml -f docker-compose.test-db.yml -f docker-compose.test.yml $@
