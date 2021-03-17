f51d6321657910860e81818fe3eb091ad8057f1d
Ben Keller <bjkeller@uw.edu>
2021-03-17 14:35:35 -0700

    Add script to add grants to enable mysqldump

---
+++ docker/mysql_init/grant_aquarium_development.sql
@@ -0,0 +1,4 @@
+ GRANT ALL ON `aquarium_development`.* TO 'aquarium'@'%' ;
+ GRANT PROCESS ON *.* TO 'aquarium'@'%' ;
+ GRANT SELECT, SHOW VIEW, RELOAD, REPLICATION CLIENT, EVENT, TRIGGER ON *.* TO 'aquarium'@'%';
+ GRANT LOCK TABLES ON *.* TO 'aquarium'@'%';
