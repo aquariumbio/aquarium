933067fa22934a612bddbe6addf643b6d0a51537
Ben Keller <bjkeller@uw.edu>
2021-03-30 16:12:29 -0700

    Reorganize production entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,6 +8,22 @@ set -euxo pipefail
 unset BUNDLE_PATH
 unset BUNDLE_BIN
 
+_clean_up_stray_server() {
+    # see https://stackoverflow.com/questions/35022428/rails-server-is-still-running-in-a-new-opened-docker-container/38732187#38732187
+    if [ -f tmp/pids/server.pid ]; then
+        echo "Removing stray server.pid"
+        rm tmp/pids/server.pid
+    fi
+}
+
+# Wait for database to start.
+_wait_for_database() {
+    echo "waiting for database to respond"
+    while ! nc -z ${DB_HOST:-'db'} ${DB_PORT:-3306}; do
+        sleep 1 # wait for 1 second before check again
+    done
+}
+
 # Add AWS ECS local domain to container resolv.conf
 # See https://github.com/docker/ecs-plugin
 #
@@ -18,13 +34,6 @@ _add_ecs_namespace() {
     fi
 }
 
-_clean_up_stray_server() {
-    # see https://stackoverflow.com/questions/35022428/rails-server-is-still-running-in-a-new-opened-docker-container/38732187#38732187
-    if [ -f tmp/pids/server.pid ]; then
-        echo "Removing stray server.pid"
-        rm tmp/pids/server.pid
-    fi
-}
 
 # Folds the license file into 80 columns, strips off the markdown header, and
 # sends the result to standard output.
@@ -36,32 +45,15 @@ _show_license() {
     fi
 }
 
-_start_production_server() {
-    echo "Starting production Rails server"
-    _add_ecs_namespace()
-    _show_license()
-    # exec
-}
-
-# Wait for database to start.
-_wait_for_database() {
-    echo "waiting for database to respond"
-    while ! nc -z ${DB_HOST:-'db'} ${DB_PORT:-3306}; do
-        sleep 1 # wait for 1 second before check again
-    done
-}
-
 _main() {
-    echo "Running backend entrypoint script with arguments: $*"
-
+    echo "Starting production Rails server"
+    
     _clean_up_stray_server
     _wait_for_database
+    _add_ecs_namespace()
+    _show_license()
 
-    if [ $1 = "production" ]; then
-        _start_production_server
-    else
-        exec "$@"
-    fi
+    exec "$@"
 }
 
 # Run _main unless sourced
