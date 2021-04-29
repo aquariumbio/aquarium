49adb7e2f35644fe3c09d9e5e463cac82a87dfb1
Mariko Anderson <marikoa@uw.edu>
2021-04-29 13:58:04 -0700

    Add global styles for flex

---
+++ frontend/src/globalUseStyles.js
@@ -0,0 +1,85 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const globalUseStyles = makeStyles((theme) => ({
+  flex: {
+    display: '-ms-flexbox',
+    // eslint-disable-next-line no-dupe-keys
+    display: 'flex',
+    position: 'relative',
+  },
+  flexWrapper: {
+    padding: '0 16px',
+  },
+  /* Title row */
+  flexTitle: {
+    padding: '8px 0',
+    borderBottom: '2px solid #c0c0c0',
+    fontWeight: 'bold',
+  },
+  flexRow: {
+    padding: '8px 0',
+    borderBottom: '1px solid #c0c0c0',
+    '&:hover': {
+      boxShadow: '0 0 3px 0 rgba(0, 0, 0, 0.8)',
+    },
+  },
+  flexRowNested: {
+    padding: '2px 0',
+  },
+  flexCol1: {
+    flex: '1 1 0',
+    marginRight: '8px',
+    paddingLeft: '8px',
+    minWidth: '0',
+  },
+
+  flexCol2: {
+    flex: '2 1 0',
+    marginRight: '8px',
+    paddingLeft: '8px',
+    minWidth: '0',
+  },
+
+  flexCol3: {
+    flex: '3 1 0',
+    marginRight: '8px',
+    paddingLeft: '8px',
+    minWidth: '0',
+  },
+
+  flexCol4: {
+    flex: '4 1 0',
+    marginRight: '8px',
+    paddingLeft: '8px',
+    minWidth: '0',
+  },
+
+  flexColAuto: {
+    width: 'auto',
+    marginRight: '8px',
+    paddingLeft: '8px',
+    minWidth: '0',
+  },
+
+  /* Use to scale and hide columns in the title row */
+  flexColAutoHidden: {
+    width: 'auto',
+    marginRight: '8px',
+    paddingLeft: '8px',
+    minWidth: '0',
+    visibility: 'hidden',
+  },
+  show: {
+    display: 'block',
+  },
+
+  hide: {
+    display: 'none',
+  },
+
+  pointer: {
+    cursor: 'pointer',
+  },
+}));
+
+export default globalUseStyles;
