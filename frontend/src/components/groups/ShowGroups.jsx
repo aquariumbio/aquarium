b36cf8bc6cd3be8cb273e4e5cda7ed3cf7362507
gnomicosuw <gnomicos@uw.edu>
2021-05-31 09:35:21 -0700

    implement globalstyles in showgroups

--- frontend/src/components/groups/ShowGroups.jsx
+++ frontend/src/components/groups/ShowGroups.jsx
@@ -7,112 +7,18 @@ import { makeStyles } from '@material-ui/core';
 import Link from '@material-ui/core/Link';
 
 import groupsAPI from '../../helpers/api/groups';
+import globalUseSyles from '../../globalUseStyles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: 'calc(100% - 64px)',
-  },
-
-  inventory: {
-    fontSize: '0.875rem',
-    marginBottom: theme.spacing(2),
-  },
-
-  /* flex */
-  flexWrapper: {
-    padding: '0 16px',
-  },
-
-  flex: {
-    display: '-ms-flexbox',
-    // eslint-disable-next-line no-dupe-keys
-    display: 'flex',
-    position: 'relative',
-  },
-
-  /* Title row */
-  flexTitle: {
-    padding: '8px 0',
-    borderBottom: '2px solid #c0c0c0',
-  },
-
-  /* Data Row */
-  flexRow: {
-    padding: '8px 0',
-    borderBottom: '1px solid #c0c0c0',
-    '&:hover': {
-      boxShadow: '0 0 3px 0 rgba(0, 0, 0, 0.8)',
-    },
-  },
-
-  /* Column definiions */
-  flexCol1: {
-    flex: '1 1 0',
-    marginRight: '8px',
-    paddingLeft: '8px',
-    minWidth: '0',
-  },
-
-  flexCol2: {
-    flex: '2 1 0',
-    marginRight: '8px',
-    paddingLeft: '8px',
-    minWidth: '0',
-  },
-
-  flexCol3: {
-    flex: '3 1 0',
-    marginRight: '8px',
-    paddingLeft: '8px',
-    minWidth: '0',
-  },
-
-  flexCol4: {
-    flex: '4 1 0',
-    marginRight: '8px',
-    paddingLeft: '8px',
-    minWidth: '0',
-  },
-
-  flexColAuto: {
-    width: 'auto',
-    marginRight: '8px',
-    paddingLeft: '8px',
-    minWidth: '0',
-  },
-
-  /* Use to scale and hide columns in the title row */
-  flexColAutoHidden: {
-    width: 'auto',
-    marginRight: '8px',
-    paddingLeft: '8px',
-    minWidth: '0',
-    visibility: 'hidden',
-  },
-
-  show: {
-    display: 'block',
-  },
-
-  hide: {
-    display: 'none',
-  },
-
-  pointer: {
-    cursor: 'pointer',
-  },
-
-  spanLink: {
-    cursor: 'pointer',
-    color: '#136390',
-    '&:hover': {
-      textDecoration: 'underline',
-    },
+    width: '100%',
   },
 }));
 
 const ShowGroups = ({ groups }) => {
   const classes = useStyles();
+  const globalClasses = globalUseSyles();
 
   const handleDelete = async (group) => {
     const response = await groupsAPI.delete(group.id);
@@ -130,30 +36,30 @@ const ShowGroups = ({ groups }) => {
 
   return (
     <>
-      <div className={classes.flexWrapper}>
-        <div className={`${classes.flex} ${classes.flexTitle}`}>
-          <Typography className={classes.flexCol1}><b>Name</b></Typography>
-          <Typography className={classes.flexCol3}><b>Description</b></Typography>
-          <Typography className={classes.flexColAutoHidden}>Edit</Typography>
-          <Typography className={classes.flexColAutoHidden}>Delete</Typography>
+      <div className={globalClasses.flexWrapper}>
+        <div className={`${globalClasses.flex} ${globalClasses.flexTitle}`}>
+          <Typography className={globalClasses.flexCol1}><b>Name</b></Typography>
+          <Typography className={globalClasses.flexCol3}><b>Description</b></Typography>
+          <Typography className={globalClasses.flexColAutoHidden}>Edit</Typography>
+          <Typography className={globalClasses.flexColAutoHidden}>Delete</Typography>
         </div>
 
         {groups.map((group) => (
-          <div className={`${classes.flex} ${classes.flexRow}`} key={`object_${group.id}`}>
-            <Typography className={classes.flexCol1}>
+          <div className={`${globalClasses.flex} ${globalClasses.flexRow}`} key={`object_${group.id}`}>
+            <Typography className={globalClasses.flexCol1}>
               <Link data-cy={`show_${group.id}`} component={RouterLink} to={`/groups/${group.id}/show`}>{group.name}</Link>
             </Typography>
-            <Typography className={classes.flexCol3}>
+            <Typography className={globalClasses.flexCol3}>
               {group.description}
             </Typography>
-            <Typography className={classes.flexColAuto}>
+            <Typography className={globalClasses.flexColAuto}>
               {/* TODO: change to iconButton when available */}
               <Link data-cy={`edit_${group.id}`} component={RouterLink} to={`/groups/${group.id}/edit`}>Edit</Link>
             </Typography>
-            <Typography className={classes.flexColAuto}>
+            <Typography className={globalClasses.flexColAuto}>
               {/* TODO: change to iconButton when available */}
               {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-              <Link data-cy={`delete_${group.id}`} className={classes.spanLink} onClick={() => handleDelete(group)}>Delete</Link>
+              <Link data-cy={`delete_${group.id}`} className={globalClasses.pointer} onClick={() => handleDelete(group)}>Delete</Link>
             </Typography>
           </div>
         ))}
