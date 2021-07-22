f8881260718b44bbf1177d2630654464f11fb1a7
Ben Keller <bjkeller@uw.edu>
2021-07-22 07:47:36 -0700

    Change parameterization

--- frontend/production-entrypoint.sh
+++ frontend/production-entrypoint.sh
@@ -5,8 +5,6 @@ set -euo pipefail
 # environment variables. These are instantiated in react build, so to preserve
 # ability to parameterize the built environment, use an external file 
 # config.json with *only* the variables needed.
-#
-# This script requires that jo be installed in the Dockerfile
 
 if [ -z ${BUILD_ROOT+x} ]; then
     echo "ERROR: BUILD_ROOT is not set";
@@ -23,8 +21,9 @@ if [ -z ${REACT_APP_BACKEND_PORT+x} ]; then
     exit 2
 fi
 
-# TODO: check this path is correct
-rm -f $BUILD_ROOT/helpers/api/config.json
-jo REACT_APP_BACKEND=$REACT_APP_BACKEND REACT_APP_BACKEND_PORT=$REACT_APP_BACKEND_PORT > $BUILD_ROOT/helpers/api/config.json
+for file in $BUILD_ROOT/static/js/*.js;
+do
+    (cat $file | envsubst '$REACT_APP_BACKEND,$REACT_APP_BACKEND_PORT' > $file.temp; mv $file.temp $file);
+done
 
 exec "$@"
