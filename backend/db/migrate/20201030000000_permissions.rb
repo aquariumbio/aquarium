58848bd0e2104f4b71f715495a9b1e96639b8317
Aquadoc <info@aquarium.com>
2021-07-28 12:51:00 -0700

    Begin fixing migrations

--- backend/db/migrate/20201030000000_permissions.rb
+++ backend/db/migrate/20201030000000_permissions.rb
@@ -1,43 +1,47 @@
 # typed: false
 class Permissions < ActiveRecord::Migration[4.2]
   def change
-    add_column :users, :permission_ids, :string, :default => "."
+    unless column_exists? :users, :permission_ids
+      add_column :users, :permission_ids, :string, :default => "."
+    end
 
     # CREATE ROLES TABLE
-    create_table :permissions do |t|
-      t.string :name, :default => '', :unique => true
-      t.integer :sort
+    unless table_exists?(:permissions)
+      create_table :permissions do |t|
+        t.string :name, :default => '', :unique => true
+        t.integer :sort
 
-      t.timestamps
+        t.timestamps
+      end
     end
 
     change_column_null :permissions, :name, false
     add_index :permissions, :name, unique: true if !index_exists?(:permissions, :name)
 
     # POPULATE ROLES TABLE
-    execute <<-SQL
-      INSERT INTO `permissions`(`id`,`name`,`sort`,`created_at`,`updated_at`) VALUES
-      ( '1', 'admin', '1', '2020-01-01 00:00:00', '2020-01-01 00:00:00' ),
-      ( '2', 'manage', '2', '2020-01-01 00:00:00', '2020-01-01 00:00:00' ),
-      ( '3', 'run', '3', '2020-01-01 00:00:00', '2020-01-01 00:00:00' ),
-      ( '4', 'design', '4', '2020-01-01 00:00:00', '2020-01-01 00:00:00' ),
-      ( '5', 'develop', '5', '2020-01-01 00:00:00', '2020-01-01 00:00:00' ),
-      ( '6', 'retired', '6', '2020-01-01 00:00:00', '2020-01-01 00:00:00' );
-    SQL
+      Permission.find_or_create_by( id: 1, name: 'admin', sort: 1 )
+      Permission.find_or_create_by( id: 2, name: 'manage',  sort: 2 )
+      Permission.find_or_create_by( id: 3, name: 'run',     sort: 3 )
+      Permission.find_or_create_by( id: 4, name: 'design',  sort: 4 )
+      Permission.find_or_create_by( id: 5, name: 'develop', sort: 5 )
+      Permission.find_or_create_by( id: 6, name: 'retired', sort: 6 )
 
     # CREATE USER_TOKENS TABLE IN SQL TO SUPPORT THE COMPOSITE KEY
-    execute <<-SQL
-      CREATE TABLE `user_tokens`(
-        `user_id` Int( 0 ) NOT NULL,
-        `token` VarChar( 128 ) NOT NULL,
-        `created_at` DateTime NOT NULL,
-        `updated_at` DateTime NOT NULL,
-        `ip` VarChar( 18 ) NOT NULL,
-        `timenow` DateTime NOT NULL,
-        PRIMARY KEY ( `ip`, `token` ) );
-    SQL
+    unless table_exists?(:permissions)
+      execute <<-SQL
+        CREATE TABLE `user_tokens`(
+          `user_id` Int( 0 ) NOT NULL,
+          `token` VarChar( 128 ) NOT NULL,
+          `created_at` DateTime NOT NULL,
+          `updated_at` DateTime NOT NULL,
+          `ip` VarChar( 18 ) NOT NULL,
+          `timenow` DateTime NOT NULL,
+          PRIMARY KEY ( `ip`, `token` ) );
+      SQL
 
-    add_index       :user_tokens, :user_id                     if !index_exists?(:plans, :budget_id)
-    add_foreign_key :user_tokens, :users, on_delete: :cascade  rescue nil
+      add_index       :user_tokens, :user_id                     if !index_exists?(:plans, :budget_id)
+      add_foreign_key :user_tokens, :users, on_delete: :cascade  rescue nil
+    end
   end
 end
+
